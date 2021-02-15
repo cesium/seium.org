76356c929ef1f5aa5e8e71e49b58f30e7944df4d
Nelson Estevão <nelsonmestevao@gmail.com>
2021-02-15 19:42:51 +0000

    Fix errors

--- src/components/moonstone/Header.js
+++ src/components/moonstone/Header.js
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <div className="header" style={props.style}>
-      <h3>{props.title}</h3>
+      <h3 className="moonstone-title header-3">{props.title}</h3>
       <div className="children">{props.children}</div>
     </div>
   );
