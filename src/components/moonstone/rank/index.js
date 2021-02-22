7aea273c52ce182aecad31a74e3a2e82594a8899
Jessica Lemos <jessica.andreia96@gmail.com>
2021-02-22 14:26:21 +0000

    Fix days API call

--- src/components/moonstone/rank/index.js
+++ src/components/moonstone/rank/index.js
@@ -8,6 +8,7 @@ import { useUser } from "../context/user";
 export default function Index(props) {
   const [rank, setRank] = useState([]);
   const [rank8, setRank8] = useState([]);
+  const [rankType, setRankType] = useState([]);
   const { user, dispatch } = useUser();
 
   var d = new Date();
@@ -19,32 +20,36 @@ export default function Index(props) {
   let processRank = (rank) => {
     setRank(rank);
     const rank8 = rank.slice(0, 8);
-    let checkownertop = rank8.filter((element) => element.nickname == user.nickname);
+    let checkownertop = rank8.filter((element) => element.name == user.name);
     if (checkownertop.length > 0) {
       setRank8(rank8);
     } else {
-      let ownerentry = rank.filter((element) => element.nickname == user.nickname);
+      let ownerentry = rank.filter((element) => element.name == user.name);
       let finalrank = rank8.concat(ownerentry);
       setRank8(finalrank);
     }
   };
 
   useEffect(async () => {
-    const { data } = await API.get(`/api/v1/leaderboard/${date}`);
-    const rank = data.data;
-    processRank(rank);
-  }, []);
+    if (rankType.length === 0 || rankType === "leaderboard") {
+      const { data } = await API.get(`/api/v1/leaderboard/${date}`);
+      const rank = data.data;
+      processRank(rank);
+    } else if (rankType === "halloffame") {
+      const { data } = await API.get(`/api/v1/leaderboard`);
+      const rank = data.data;
+      processRank(rank);
+    }
+  }, [props.day, rankType]);
 
   const hall = useCallback(async () => {
-    const { data } = await API.get(`/api/v1/leaderboard`);
-    const rank = data.data;
-    processRank(rank);
+    const rankType = "halloffame";
+    setRankType(rankType);
   }, []);
 
   const learder = useCallback(async () => {
-    const { data } = await API.get(`/api/v1/leaderboard/${date}`);
-    const rank = data.data;
-    processRank(rank);
+    const rankType = "leaderboard";
+    setRankType(rankType);
   }, []);
 
   const handleFormSubmit = () => {
@@ -91,12 +96,12 @@ export default function Index(props) {
         ? rank8.map((item, index) => (
             <RankPlaces
               rank={
-                item.nickname === user.nickname
-                  ? rank.findIndex((x) => x.nickname === user.nickname) + 1
+                item.name === user.name
+                  ? rank.findIndex((x) => x.name === user.name) + 1
                   : index + 1
               }
-              owner={item.nickname === user.nickname ? "owner" : null}
-              username={item.name ? item.name : item.nickname}
+              owner={item.name === user.name ? "owner" : null}
+              username={item.nickname ? item.nickname : item.name}
               badges={item.badges}
             ></RankPlaces>
           ))
