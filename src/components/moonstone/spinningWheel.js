c8015a44161bf739b655f7ea0f1e1da0ee270a8c
João Vilaça <machadovilaca@gmail.com>
2021-02-22 00:28:41 +0000

    Add working wheel

--- src/components/moonstone/spinningWheel.js
+++ src/components/moonstone/spinningWheel.js
@@ -1,25 +1,72 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+
 import Header from "./Header";
-import Button from "./Button";
-import Achievement from "./Achievement";
+import UserAchievementsItems from "./User/UserAchievementsItems";
+import API from "../../utils/api";
+import { useUser } from "./context/user";
 
-function getrotateDegree(min, max) {
-  return Math.random() * (max - min) + min;
-}
+import WheelComponent from "./wheel/WheelComponent";
+import "react-wheel-of-prizes/dist/index.css";
 
-const SpinningWheel = () => {
-  const [rotate, setRotate] = useState(getrotateDegree(100, 1300));
+const getSegments = (prizes) => {
+  const names = [];
 
-  const Wheel = styled.div`
-    animation: spin 5s cubic-bezier(0.45, -0.37, 0, 1);
-    animation-fill-mode: forwards;
-    @keyframes spin {
-      100% {
-        transform: rotate(${rotate + "deg"});
-      }
+  for (const { name } of prizes) {
+    names.push(name);
+  }
+
+  return names;
+};
+
+const segColors = [
+  "#0bfeb9",
+  "#1afdbc",
+  "#27fdc0",
+  "#32fdc3",
+  "#3efdc7",
+  "#4bfdca",
+  "#56fdce",
+  "#63fdd1",
+  "#6ffdd5",
+  "#7afdd8",
+  "#86fedc",
+  "#93fedf",
+  "#9ffee3",
+  "#adfee7",
+  "#b7feea",
+  "#c3feed",
+  "#cffef1",
+  "#d1fef1",
+  "#d3fef2",
+  "#d6fef3",
+];
+
+const SpinningWheel = (props) => {
+  const { user } = useUser();
+  const [info, setInfo] = useState({ prizes: undefined, state: 0, tokens: 0 });
+
+  useEffect(() => {
+    async function fetchData() {
+      const {
+        data: { data: prizes },
+      } = await API.get(`/api/v1/roulette/prizes`);
+
+      const {
+        data: { data: attendee },
+      } = await API.get(`/api/v1/attendees/${user.id}`);
+
+      setInfo({ prizes, state: info.state, tokens: attendee.token_balance });
     }
-  `;
+    fetchData();
+  }, []);
+
+  const incrementState = () => {
+    setInfo({
+      prizes: info.prizes,
+      state: info.state + 1,
+      tokens: info.tokens
+    });
+  };
 
   return (
     <div className="spinningWheel">
@@ -27,20 +74,22 @@ const SpinningWheel = () => {
         <div
           style={{ display: "flex", whiteSpace: "nowrap", paddingTop: "7%" }}
         >
-          <Achievement text="💰170 Tokens " />
-          <Achievement text="🥇68 Badges " />
+          <UserAchievementsItems state={info.state} />
         </div>
       </Header>
-      <Wheel className="wheel"></Wheel>
-      <div className="union"></div>
-      <Button
-        style={{ marginTop: "20px" }}
-        width="208px"
-        inner="15 tokens 💰"
-        onClick={() => setRotate((curr) => getrotateDegree(100, 1300))}
-      >
-        SPIN THE WHEEL
-      </Button>
+      {info.prizes === undefined ? (
+        ""
+      ) : (
+        <WheelComponent
+          disabled={info.tokens < 30}
+          setRolling={props.setRolling}
+          segments={getSegments(info.prizes)}
+          segColors={segColors}
+          onFinished={(winner) => incrementState()}
+          primaryColor="black"
+          contrastColor="black"
+        />
+      )}
     </div>
   );
 };
