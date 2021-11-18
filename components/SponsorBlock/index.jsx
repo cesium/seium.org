7e77b075976ea3fae88f95db91469aaa08b23348
Rui Oliveira <rpoliveira02@gmail.com>
2021-11-18 22:34:57 +0000

    Added Exclusive/Gold label on top of sponsors

--- components/SponsorBlock/index.jsx
+++ components/SponsorBlock/index.jsx
@@ -2,6 +2,11 @@ import styles from './style.module.css';
 
 export default function SponsorBlock(props) {
     return (
-        <div className={styles.testDiv} style={{backgroundColor: props.color}}></div>
+        <div className={styles.wrapper}>
+            <div className={styles.sponsorType}>
+                <h3>{props.sponsorType}</h3>
+            </div>
+            <div className={styles.testDiv} style={{backgroundColor: props.color}}></div>
+        </div>
     );
 }
--- components/Sponsors/index.jsx
+++ components/Sponsors/index.jsx
@@ -22,16 +22,16 @@ export default function Sponsors(props) {
     return (
         <>
             <div className={styles.temp}>
-                <SponsorTab tabName="Exclusive" selected={val} onSelect={changeValLeft}></SponsorTab>
-                <SponsorTab tabName="Silver" selected={!val} onSelect={changeValRight}></SponsorTab>
+                <SponsorTab tabName="Exclusive&Gold" selected={val} onSelect={changeValLeft}></SponsorTab>
+                <SponsorTab tabName="Silver&Bronze" selected={!val} onSelect={changeValRight}></SponsorTab>
             </div>
 
             <div className={styles.blockWrapper}>
-                <div className={styles.leftBlock}>
-                    <SponsorBlock color="red"></SponsorBlock>
+                <div className={styles.leftBlock}>                  
+                    <SponsorBlock sponsorType={val ? "Exclusive" : "Silver"} color="red" left={true}></SponsorBlock>
                 </div>
                 <div className={styles.rightBlock}>
-                    <SponsorBlock color="green"></SponsorBlock>
+                    <SponsorBlock sponsorType={val ? "Gold" : "Bronze"} color="green" left={false}></SponsorBlock>
                 </div>
                 
             </div>
