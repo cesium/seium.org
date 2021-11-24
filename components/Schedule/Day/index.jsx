7de86133f5f64fe94294462517598241543224ec
tiago-bacelar <pereira.migueltiago@gmail.com>
2021-11-24 22:48:03 +0000

    Added dynamic schedule functionality

--- components/Schedule/Day/index.jsx
+++ components/Schedule/Day/index.jsx
@@ -1,7 +1,11 @@
 import styles from './style.module.css';
 
-export default function Day(props) {
-
+export default function Day(props)
+{
+    const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Oct", "Nov", "Dec" ];
+    let date = props.date.split("/");
+    let date_string = date[2] + " " + months[date[1] - 1];
+    let date_descriptor = "Today";  //TODO
 
     return (
         <div className={styles.wrapper}>
@@ -10,8 +14,8 @@ export default function Day(props) {
             </div>
 
             <div className={`-mt-10 ${styles.dateWrapper}`}>
-                <h5 className="text-3xl text-blue-500 font-bold">23 Feb</h5>
-                <h2 className="font-bold text-8xl text-white">Today</h2>
+                <h5 className="text-3xl text-blue-500 font-bold"> { date_string } </h5>
+                <h2 className="font-bold text-8xl text-white"> { date_descriptor } </h2>
             </div>
             
             <div className={`${styles.rightArrow} ${styles.arrowWrapper}`}>
