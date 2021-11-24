46130a69430d95721b84d1a1f08c6dceb2ae5d87
tiago-bacelar <pereira.migueltiago@gmail.com>
2021-11-24 22:48:03 +0000

    Added dynamic schedule functionality

--- components/landing/sections/Schedule/Table/index.jsx
+++ components/landing/sections/Schedule/Table/index.jsx
@@ -1,14 +1,11 @@
 import Block from './Block';
 
-export default function Table() {
-    return (
-        <div>
-            <Block startTime="10:00" endTime="11:00" activityType="Talk" description="Uma talk bué fixe" author="Rui Oliveira" coffeeBreak={false}/>
-            <Block startTime="10:00" endTime="11:00" activityType="Coffee Break" coffeeBreak={true}/>
-            <Block startTime="10:00" endTime="11:00" activityType="Talk" description="Uma talk bué fixe" author="Rui Oliveira" coffeeBreak={false}/>
-            <Block startTime="10:00" endTime="11:00" activityType="Talk" description="Uma talk bué fixe" author="Rui Oliveira" coffeeBreak={false}/>
-            <Block startTime="10:00" endTime="11:00" activityType="Talk" description="Uma talk bué fixe" author="Rui Oliveira" coffeeBreak={false}/>
-            <Block startTime="10:00" endTime="11:00" activityType="Talk" description="Uma talk bué fixe" author="Rui Oliveira" coffeeBreak={false}/>
-        </div>
-    );
+export default function Table(props)
+{
+    const schedule = require('./schedule.json');
+    
+    if (schedule[props.date] == undefined)
+        return <Block/>;
+    else
+        return schedule[props.date].map((activity) => new Block(activity));
 }
--- components/landing/sections/Schedule/index.jsx
+++ components/landing/sections/Schedule/index.jsx
@@ -1,15 +1,79 @@
 import Table from './Table';
 import Day from './Day';
 
-export default function Schedule() {
+import { useState } from 'react'
+
+function isAfter(date1, date2) // equivalent to date1 > date2
+{
+    const arr1 = date1.split("/");
+    const arr2 = date2.split("/");
+    const year1 = parseInt(arr1[0]), month1 = parseInt(arr1[1]), day1 = parseInt(arr1[2]);
+    const year2 = parseInt(arr2[0]), month2 = parseInt(arr2[1]), day2 = parseInt(arr2[2]);
+
+    return year1 > year2 || month1 > month2 || day1 > day2;
+}
+
+function leapYear(year)
+{
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+function addDate(date, days) //date is a string "yyyy/mm/dd"
+{
+    //Assume days < 28 and the year won't need changing
+    const arr = date.split("/");
+    const year = parseInt(arr[0]);
+    let month = parseInt(arr[1]);
+    let day = parseInt(arr[2]);
+
+    const month_len = [ 31, leapYear(parseInt(arr[0])) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+    day += days;
+
+    if (day <= 0)
+    {
+        month--;
+        day += month_len[month - 1];
+    }
+    else if (day > month_len[month - 1])
+    {
+        day -= month_len[month - 1];
+        month++;
+    }
+
+    return year + "/" + month + "/" + day;
+}
+
+export default function Schedule()
+{
+    const default_date = "2022/2/23"; //TODO: define as current day
+    const min_date = "2022/2/23";
+    const max_date = "2022/2/27";
+    const [date, updateDate] = useState(default_date);
+
+    const table = <Table date = { date } />;
+
+    const previous_day = function(){
+        const new_date = addDate(date, -1);
+        if (!isAfter(min_date, new_date) && !isAfter(new_date, max_date))
+            updateDate(new_date);
+    };
+
+    const next_day = function(){
+        const new_date = addDate(date, 1);
+        if (!isAfter(min_date, new_date) && !isAfter(new_date, max_date))
+            updateDate(new_date);
+    };
+
+    const day = <Day date = { date } previousDay = { previous_day } nextDay = { next_day } />;
+
     return (
         <div className="spacing grid grid-cols-2 bg-medium_blue pt-20">
             <div className="">
-                <Day/>
+                { day }
             </div>
 
             <div className="">
-                <Table/>
+                { table }
             </div>
         </div>
     );
