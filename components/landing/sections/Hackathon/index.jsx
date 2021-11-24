7857fc1c6c195a1f3bafeb6f64642f3462a9cf19
Rui Oliveira <rpoliveira02@gmail.com>
2021-11-24 21:44:04 +0000

    Made Hackathon responsive

--- components/landing/sections/Hackathon/index.jsx
+++ components/landing/sections/Hackathon/index.jsx
@@ -9,7 +9,9 @@ export default function Hackathon() {
                 </h3>
                 <h1 className="px-5 text-7xl text-white font-bold">
                     Create products, train skills and learn new technologies
-                            <span className="pl-4 align-middle"><Button text="REGISTER YOUR TEAM" /></span>
+                            <span className="pl-4 pb-8 align-middle items-center text-center content-center">
+                                <Button className="items-center" text="REGISTER YOUR TEAM" />
+                            </span>
                             <span className="text-gray-100 text-2xl pl-4 align-middle">
                                 2-5 people
                             </span>
