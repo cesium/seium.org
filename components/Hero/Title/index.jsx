92f4a291089fde76864de35b5d7ebef965f457e5
Rui Oliveira <ruioliveira02@archmachine.localdomain>
2021-11-25 22:43:44 +0000

    Restored lines animation

--- components/Hero/Title/index.jsx
+++ components/Hero/Title/index.jsx
@@ -12,16 +12,16 @@ export default function Title() {
             <h5 className="text-2xl text-aqua">
                 15-20 February 2022
             </h5>
-            <h1 className="text-9xl text-white w-4/5 relative z-50">
+            <h1 className="text-9xl text-white w-4/5 relative z-0">
                 The software engineering week is back, let's just&nbsp;
-                <span className="relative leading-none z-50 w-auto my-4 inline-block">
-                    <u className="relative bg-dark_blue h-auto inline-block" onMouseEnter={() => setHover(true)} 
+                <span className="relative leading-none z-10 w-auto my-4 inline-block">
+                    <u className="relative h-auto inline-block z-0" onMouseEnter={() => setHover(true)} 
                         onMouseLeave={() => setHover(false)}>
                             SEI
                     </u>
-                    <span className="bg-dark_blue h-auto inline-block"> &nbsp;that</span>
+                    <span className="relative h-auto inline-block z-10"> &nbsp;that</span>
                     <div className={styles.cardWrapper}>
-                        <Fade top when={hovered}>
+                        <Fade top when={hovered} distance="10px">
                             <Card className="z-0" img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
                                 <h5 className={styles.cardText}>Did you see what I did there?</h5>
                             </Card>
