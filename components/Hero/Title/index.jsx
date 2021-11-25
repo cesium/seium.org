56781531f0e76e7e6ac8ded067346b843c43fa9c
Rui Oliveira <ruioliveira02@archmachine.localdomain>
2021-11-25 22:06:06 +0000

    Fixed relative position of elements

--- components/Hero/Title/index.jsx
+++ components/Hero/Title/index.jsx
@@ -8,23 +8,23 @@ import styles from './style.module.css';
 export default function Title() {
     const [hovered, setHover] = useState(false);
     return (
-        <div className="font-bold">
+        <div className="font-bold z-50 relative">
             <h5 className="text-2xl text-aqua">
                 15-20 February 2022
             </h5>
-            <h1 className="text-9xl text-white w-4/5">
+            <h1 className="text-9xl text-white w-4/5 relative z-50">
                 The software engineering week is back, let's just&nbsp;
-                <span className="z50 relative leading-none w-32" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-                    <u className={styles.underline}>SEI</u>
+                <span className="relative leading-none z-50 w-auto my-4 inline-block" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                    <u className="relative bg-dark_blue h-auto inline-block">SEI</u>
                     <div className={styles.cardWrapper}>
                         {hovered ? 
                         <Fade top>
-                            <Card img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
+                            <Card className="z-0" img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
                                 <h5 className={styles.cardText}>Did you see what I did there?</h5>
                             </Card>
                         </Fade> : <></>}
                     </div>
-                </span> that
+                </span><span className="relative bg-dark_blue z-50 h-auto inline-block">&nbsp;that</span>
             </h1>
             
                 
