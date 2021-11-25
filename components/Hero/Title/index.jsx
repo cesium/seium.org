fa34f6ec4b71237f67c4deeca471932c7856b0ae
Rui Oliveira <ruioliveira02@archmachine.localdomain>
2021-11-25 22:31:21 +0000

    Added fade out

--- components/Hero/Title/index.jsx
+++ components/Hero/Title/index.jsx
@@ -14,17 +14,20 @@ export default function Title() {
             </h5>
             <h1 className="text-9xl text-white w-4/5 relative z-50">
                 The software engineering week is back, let's just&nbsp;
-                <span className="relative leading-none z-50 w-auto my-4 inline-block" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-                    <u className="relative bg-dark_blue h-auto inline-block">SEI</u>
+                <span className="relative leading-none z-50 w-auto my-4 inline-block">
+                    <u className="relative bg-dark_blue h-auto inline-block" onMouseEnter={() => setHover(true)} 
+                        onMouseLeave={() => setHover(false)}>
+                            SEI
+                    </u>
+                    <span className="bg-dark_blue h-auto inline-block"> &nbsp;that</span>
                     <div className={styles.cardWrapper}>
-                        {hovered ? 
-                        <Fade top>
+                        <Fade top when={hovered}>
                             <Card className="z-0" img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
                                 <h5 className={styles.cardText}>Did you see what I did there?</h5>
                             </Card>
-                        </Fade> : <></>}
+                        </Fade> 
                     </div>
-                </span><span className="relative bg-dark_blue z-50 h-auto inline-block">&nbsp;that</span>
+                </span>
             </h1>
             
                 
