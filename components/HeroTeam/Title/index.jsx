db5a950763ba5a77eaabfff0bf1f22121f38b2de
David Machado <machado.david190@gmail.com>
2021-12-02 23:07:26 +0000

    Organization creation

--- components/HeroTeam/Title/index.jsx
+++ components/HeroTeam/Title/index.jsx
@@ -4,11 +4,7 @@ export default function Title() {
             <h5 className="text-2xl text-dark_blue">
                 Team
             </h5>
-<<<<<<< refs/remotes/origin/ff/refactoring
             <h1 className="text-white text-6xl lg:text-8xl 2xl:text-9xl md:w-4/5 relative z-0">
-=======
-            <h1 className="text-9xl text-white w-4/5 relative z-0">
->>>>>>> Team
                 CeSIUM has teamed up with the best team possible.
             </h1>
         </div>
