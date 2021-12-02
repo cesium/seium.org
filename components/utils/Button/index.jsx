8ee62f41cac409ca0dcfcd2cb90280a8a276d4f2
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-12-02 10:13:37 +0000

    :art: Add badgedex page

--- components/utils/Button/index.jsx
+++ components/utils/Button/index.jsx
@@ -1,4 +1,4 @@
-export default function Button({ text, type, bg_color, fg_color, border, padding}) {
+export default function Button({ text, type, bg_color, fg_color, border, padding, onClick}) {
     return (
         <button type={type} className={`bg-${bg_color} text-${fg_color} w-full rounded-full text-center items-center px-${padding} py-1 border border-${border} shadow-sm text-sm`}>
             { text }
