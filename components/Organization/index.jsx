7a7cff5f33b4256268cc5a996f4420e8fedb7632
David Machado <machado.david190@gmail.com>
2021-12-03 01:02:02 +0000

    Fix Organization Add FindUs

--- components/Organization/index.jsx
+++ components/Organization/index.jsx
@@ -21,7 +21,7 @@ function Animation(props) {
       <div ref={observe} style={{ height: "25px" }}>
         {inView ?
           <Fade bottom>
-            <div className={`-mt-6 ${styles.cardfooter}`}>
+            <div className={`-mt-6 ${styles.cardfooter} border-b-2 border-white`}>
               <Card img="/images/mascot-footer.svg" alt="MascotFooter" inverted={false}>
                 I am also very important to the team. Actually I should be in first place
               </Card>
@@ -50,35 +50,49 @@ export default function Organization() {
     return (
         <section className="py-20 grid grid-cols-1 lg:grid-cols-2 gap-x-32 gap-y-8 bg-medium_light_blue spacing">
             <div className="text-white">
-                <h2 className="text-5xl font-bold">
+                <h2 className="text-6xl font-bold mb-4">
                     Organization
                 </h2>
                 <p>They walk around, full of work, gathering speakers, attracting partners and giving their imaginations wings, all for this to be your favorite week.</p>
             </div>
-            <div className="grid grid-cols-2 gap-8">
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+            <div>
+                <h3 className="text-white font-bold mb-4">Tech Team</h3>
+                <div className="grid grid-cols-2 gap-8 justify-items-center">
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                </div>
             </div>
-            <div className="grid grid-cols-2 gap-8">
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+            
+            <div>
+                <h3 className="text-white font-bold mb-4">Support Team</h3>
+                <div className="grid grid-cols-2 gap-8 justify-items-center">
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                </div>
             </div>
             <div className="hidden lg:flex justify-center items-center">
                 <Animation></Animation>
             </div>
-            <div className="grid grid-cols-2 gap-8">
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+
+            <div>
+                <h3></h3>
+                <div className="grid grid-cols-2 gap-8 justify-items-center">
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                </div>
             </div>
-            <div className="grid grid-cols-2 gap-8">
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
-                <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+            <div>
+                <h3 className="text-white font-bold mb-4">Partners Team</h3>
+                <div className="grid grid-cols-2 gap-8 justify-items-center">
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                    <Member id="joaooliveira" name="João Oliveira" job="CEO and Founder" company="Fake Company"/>
+                </div>
             </div>
         </section>
     )
