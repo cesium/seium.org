f82f0ef38037ce6c95f683070034973e3c34ad93
David Machado <machado.david190@gmail.com>
2021-12-03 01:02:02 +0000

    Fix Organization Add FindUs

--- components/FindUs/index.jsx
+++ components/FindUs/index.jsx
@@ -1,3 +1,20 @@
 export default function FindUs() {
-    return <> </>
+    return (
+        <section className="flex flex-col md:flex-row md:justify-between bg-medium_light_blue py-20 spacing">
+            <div className="flex flex-col text-white mb-10">
+                <h2 className="font-bold text-6xl mb-2">How to find us</h2>
+
+                <p className="mb-8">The SEI is free for participants and is organized by volunteers from CeSIUM.</p>
+
+                <p className="font-bold mb-2">Centro de Estudantes de Engenharia Informática</p>
+                <ul className="list-inside list-disc">
+                    <li className="">E-mail: cesium@di.uminho.pt</li>
+                    <li className="">Phone: +351 253 604 448</li>
+                </ul>
+            </div>
+            <div>
+                <div className="h-72 w-96 bg-dark_blue"></div>
+            </div>
+        </section>
+    )
 }
