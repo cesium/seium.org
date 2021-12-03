db7eedea0526000bf784204416f449a614cc59d3
David Machado <machado.david190@gmail.com>
2021-12-03 01:02:02 +0000

    Fix Organization Add FindUs

--- components/Organization/Member/index.jsx
+++ components/Organization/Member/index.jsx
@@ -7,7 +7,7 @@ export default function Member({ id, name, job, company }) {
   return (
     <div className="text-white">
       <Image src={`/images/speakers/${id}.png`} width="210" height="210"/>
-      <p className="text-md font-bold"> { name } </p>
+      <p className="text-md font-bold mb-1"> { name } </p>
       <div className="flex">
         <a className="w-4 mr-3" target="_blank" href="https://www.facebook.com/SEI.UMinho/">
           <FontAwesomeIcon icon={faGithubAlt} />
