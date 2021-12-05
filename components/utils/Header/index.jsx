9c5855215f62a5040b34cb5863621767698e99f1
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-12-05 19:25:08 +0000

    Add Map

--- components/utils/Header/index.jsx
+++ components/utils/Header/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function Header(){
+export default function Header() {
     return (
         <Head>
             <title>
@@ -8,6 +8,7 @@ export default function Header(){
             </title>
             <link rel="preload" href="/fonts/Inter/Inter-Regular.ttf" as="font-regular" />
             <link rel="preload" href="/fonts/Inter/Inter-Black.ttf" as="font-black" />
+            <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css" />
         </Head>
     );
 }
