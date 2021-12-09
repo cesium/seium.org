d268656ee1dd6d5b72939f5676a68c8f3a49e4d1
Rui Oliveira <rpoliveira02@gmail.com>
2021-12-09 20:41:33 +0000

    Did hero

---
+++ components/hackathon/Hero/Pitch/index.jsx
@@ -0,0 +1,19 @@
+import Button from "/components/utils/Button";
+
+export default function Pitch() {
+    return (
+        <div className="w-full lg:w-9/12 xl:9/12 text-white">
+            <h2 className="text-4xl font-bold">
+                Hackathon 2022
+            </h2>
+            <div className="mt-8">
+                <p className="text-white">
+                    It will start at the end of the 30th of February and will run until the end of the afternoon of the 31th.
+                    There are more than 40 hours to have fun programming! And, if you need to, you can always count on the help of our mentors.
+                    Each team may have between 3 and 5 elements. If you are unable to complete a team, you can always register individually or in pairs, 
+                    and we will try to group you with other participants.
+                </p>
+            </div>
+        </div>
+    );
+}
