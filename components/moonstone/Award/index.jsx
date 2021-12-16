91e1c2e19ac67f3e2e4b360ce402cdbe13ef81e6
Rui Oliveira <rpoliveira02@gmail.com>
2021-12-16 22:09:08 +0000

    Added fonts

--- components/moonstone/Award/index.jsx
+++ components/moonstone/Award/index.jsx
@@ -7,15 +7,15 @@ export default function Award({image, cost, available, message, enabled}) {
                 <Image src={image} width="200" height="200"></Image>
                 <div className="w-auto mt-8">
                     <button className="block m-auto bg-aqua rounded-full w-64 h-20">
-                        <p className="font-bold">REDEEM</p>
-                        <p>{cost} tokens💰</p>
+                        <p className="font-bold font-ibold">REDEEM</p>
+                        <p className="font-iregular">{cost} tokens💰</p>
                     </button>
                 </div>
             </div>
             
             <div className="text-center mt-6">
-                <p className="font-bold">{available} available</p>
-                <p>{message}</p>
+                <p className="font-bold font-ibold">{available} available</p>
+                <p className="font-iregular">{message}</p>
             </div>
         </div>
     );
