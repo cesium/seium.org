d064489219f728574d656ece5bac30d833249504
tiago-bacelar <pereira.migueltiago@gmail.com>
2021-12-16 21:24:46 +0000

    Copied everything from tb/dynamic-schedule in order to 'rebase'. Beginning work on the detailed view

--- components/landing/home/Schedule/Table/Block/index.jsx
+++ components/landing/home/Schedule/Table/Block/index.jsx
@@ -1,16 +1,35 @@
 import styles from './style.module.css';
+import Image from 'next/image';
+import Button from '/components/utils/Button';
+import { useState, useEffect } from 'react'
 
 export default function Block(props) {
-    return (
-        <div className={`${styles.topLine} ${props.coffeeBreak ? styles.noSpace : styles.spaceBehind}`}>
-            <div className={styles.imgWrapper} style={{ display: (props.coffeeBreak ? "flex" : "none") }}>
+
+    const id = `${props.date}-${props.id}`;
+
+    const block = (
+        <div id={id} className={`${styles.block} ${props.coffeeBreak ? styles.coffee : styles.notCoffee}`}>
+            <div className={`${styles.imgWrapper} ${props.coffeeBreak ? "flex" : "hidden"}`}> 
+                <Image src="/images/Coffee.svg" layout="fill"/>
             </div>
-            <p className={`font-iextrabold ${styles.paragraph}`}>{props.startTime}-{props.endTime}</p>
-            <div className={`flex flex-row ${styles.paragraph}`}>
-                <p className="font-iextrabold"> {props.activityType} </p>
-                <p className="pl-2 font-iregular"> {props.description}</p>
-            </div>
-            <p className={`font-iregular ${styles.author}`}>{props.author}</p>
+            <p className={`${styles.paragraph} ${styles.bold} ${props.coffeeBreak ? "hidden" : "flex"}`}>
+                {props.startTime}-{props.endTime}
+            </p>
+            <p className={styles.paragraph}><b>{props.activityType}</b> {props.summary}</p>
+            <p className={styles.author}>{props.author}</p>
+            <p className={styles.location}>{props.location}</p>
+            <button className={`${styles.expand} ${!props.coffeeBreak && props.detailed ? "flex" : "hidden"}`}>
+                { props.focused ? "-" : "+" }
+            </button>
         </div>
     );
+
+    if (props.coffeeBreak)
+        return block;
+    else
+        return (
+            <a className={styles.clickable} href={`agenda/#${id}`}>
+                { block }
+            </a>
+        );
 }
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Index() {
   return (
     <>
       <Hero />
-      <Schedule />
+      <Schedule detailed={false} />
       <Sponsors/>
       <Hackathon />
       <Speakers />
