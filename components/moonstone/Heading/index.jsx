5aeb3921bcbfce66a2e44107eac1b9bcaddc029f
Rui Oliveira <rpoliveira02@gmail.com>
2021-12-16 21:22:36 +0000

    Added fonts

--- components/moonstone/Heading/index.jsx
+++ components/moonstone/Heading/index.jsx
@@ -1,10 +1,10 @@
 export default function Heading({text, children}) {
     return (
         <div className="w-full mb-5 pb-3 border-b-solid border-b-2 border-black grid-cols-2 overflow-hidden">
-            <div className="col-span-1 flex float-left h-full">
-                <p className="inline-block align-middle font-bold text-2xl">{text}</p>
+            <div className="col-span-1 flex float-left h-full font-ibold">
+                <p className="inline-block align-middle font-bold text-2xl font-ibold">{text}</p>
             </div>
-            <div className="col-span-1 flex float-right">
+            <div className="col-span-1 flex float-right font-iregular">
                 {children}
             </div>
         </div>
--- pages/badge/[slug].js
+++ pages/badge/[slug].js
@@ -6,13 +6,13 @@ function BadgeOwner({user, badge, when}) {
     return (
         <div className="w-full mb-5 pb-3 border-b-solid border-b-2 border-black grid grid-cols-3">
             <div className="text-left">
-                <p className="font-bold">{user}</p>
+                <p className="font-ibold">{user}</p>
             </div>
             <div className="text-center">
-                <p>{badge}</p>
+                <p className="text-iregular">{badge}</p>
             </div>
             <div className="text-right">
-                <p className="font-bold text-aqua">{when}</p>
+                <p className="font-bold text-aqua text-ibold">{when}</p>
             </div>
         </div>
     );
@@ -22,14 +22,14 @@ export default function BadgeSlug() {
     return (
         <Dashboard>
             <div>
-                <h1 className="font-bold text-4xl sm:text-5xl">Badge Page</h1>
-                <p className="mt-2 text-md sm:text-lg">Check who already has this badge</p>
+                <h1 className="font-iextrabold text-4xl sm:text-5xl">Badge Page</h1>
+                <p className="mt-2 font-iregular text-md sm:text-lg">Check who already has this badge</p>
             </div>
 
-            <div className="grid gap-y-8 gap-x-2 grid-cols-1 md:grid-cols-2 justify-items-center mt-10">
+            <div className="grid gap-y-8 gap-x-2 grid-cols-1 lg:grid-cols-2 justify-items-center mt-10">
                 <div className="w-full">
                     <Heading text="Badge info">
-                        <span className="mt-1 mr-12">Talk Badge</span>
+                        <span className="mt-1 mr-12 font-iregular">Talk Badge</span>
                     </Heading>
 
                     <Badge/>
