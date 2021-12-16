422bc589002c61241f5d1a0ed52eed67eadf6eb4
Rui Oliveira <rpoliveira02@gmail.com>
2021-12-16 21:44:54 +0000

    Changed fonts

--- pages/profile.js
+++ pages/profile.js
@@ -12,7 +12,7 @@ export default function Profile() {
   return (
     <Dashboard href="profile" title="User Profile" description="Hi John, welcome to your profile">
       <div className="grid-cols-2 overflow-hidden">
-        <div className="col-span-1 w-full md:w-1/2 float-left">
+        <div className="mt-12 col-span-1 w-full md:w-1/2 float-left">
           <Heading text="User Profile">
             <div className="w-24">
               <Button bg_color="aqua" fg_color="white" text="Edit"></Button>
@@ -45,28 +45,28 @@ export default function Profile() {
                   bgColor="white"
                   fgColor="black"
               />
-              <a href="#" className="pl-6 text-aqua h-auto inline-block underline">Reset Password</a>
+              <a href="#" className="text-iregular pl-6 text-aqua h-auto inline-block underline">Reset Password</a>
           </Form>
         </div>
 
-        <div className="col-span-1 w-full md:w-1/2 float-right pt-6 md:pl-6">
+        <div className="mt-12 col-span-1 w-full md:w-1/2 float-right md:pl-6">
           <div>
             <Heading text="Achievements"></Heading>
 
             <div className="grid-cols-2 overflow-hidden">
-              <div className="col-span-1 float-left w-1/2">
+              <div className="col-span-1 float-left w-1/2 font-iregular">
                 💰170 Tokens
               </div>
-              <div className="col-span-1 float-left w-1/2">
+              <div className="col-span-1 float-left w-1/2 font-iregular">
                 🏅68 Badges
               </div>
             </div>
 
             <div className="grid-cols-2 overflow-hidden mt-4">
-              <div className="col-span-1 float-left w-1/2">
+              <div className="col-span-1 float-left w-1/2 font-iregular">
                 🏆16 Entries Final Draw
               </div>
-              <div className="col-span-1 float-left w-1/2">
+              <div className="col-span-1 float-left w-1/2 font-iregular">
                 🏁Level 3 Checkpoint
               </div>
             </div>
@@ -74,14 +74,14 @@ export default function Profile() {
 
           <div className="mt-10">
             <Heading text="Checkpoints"></Heading>
-            <p><b>Level 1</b> 5 companies &rarr; +10 entries</p>
-            <p><b>Level 2</b> 10 companies &rarr; +20 entries</p>
-            <p><b>Level 3</b> 15 companies &rarr; +40 entries</p>
-            <p><b>Level 4</b> 20 companies &rarr; +100 entries</p>
+            <p className="font-iregular"><b className="font-ibold">Level 1</b> 5 companies &rarr; +10 entries</p>
+            <p className="font-iregular"><b className="font-ibold">Level 2</b> 10 companies &rarr; +20 entries</p>
+            <p className="font-iregular"><b className="font-ibold">Level 3</b> 15 companies &rarr; +40 entries</p>
+            <p className="font-iregular"><b className="font-ibold">Level 4</b> 20 companies &rarr; +100 entries</p>
 
             <CheckpointTracker checkpoints={5} progress={3}/>
 
-            <p>You just need 4 more bages to go to Level 4 (and win 10+ entries to the final draw). Hurry!</p>
+            <p className="font-iregular">You just need 4 more bages to go to Level 4 (and win 10+ entries to the final draw). Hurry!</p>
             <CodeInput/>
           </div>
         </div>
