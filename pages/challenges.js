8f281ff9ac75054ae57612670c6b65068ce0e10c
Rui Oliveira <rpoliveira02@gmailcom>
2021-12-19 14:03:07 +0000

    Added Challenges

--- pages/challenges.js
+++ pages/challenges.js
@@ -1,17 +1,38 @@
 import Hero from '/components/landing/challenges/Hero';
 import Challenge from '/components/landing/challenges/Challenge';
 import Footer from '/components/landing/utils/Footer';
+import Button from '/components/utils/Button';
 
 
+function Action({text, url}) {
+  return (
+    <div className="w-40">
+      <Button href={url} text={text} fg_color="white"/>
+    </div>
+  );
+}
 
 export default function Index() {
+  const challenges = ["Today's Challenge", "CV Challenge", "Photography Challenge", "Final Challenge"];
+  const prizes = [["Alcatel One Touch", "Alcatel One Touch", "Alcatel One Touch"],["Alcatel One Touch", "Alcatel One Touch", "Alcatel One Touch"],["Alcatel One Touch", "Alcatel One Touch", "Alcatel One Touch"],["Alcatel One Touch", "Alcatel One Touch", "Alcatel One Touch"]];
+  const prizesUrls = [["", "", ""],["", "", ""],["", "", ""],["", "", ""]];
+  const descriptions = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"];
+  const extraDescriptions = ["", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam", "", ""];
+  const actionItems = [null, null, <Action text="Read The Rules" url=""/>, <Action text="Read The Rules" url=""/>];
+
   return (
     <>
       <Hero />
 
-      <Challenge></Challenge>
-
-      <Footer color="dark_blue" />
+      <div className="flex flex-col w-full bg-dark_blue px-40 pb-20 spacing">
+        {[0,1,2,3].map((i => <section id={challenges[i]}><Challenge list={challenges} title={challenges[i]} prizes={prizes[i]} prizesUrls={prizesUrls[i]} 
+                                extraDescription={extraDescriptions[i]} description={descriptions[i]} actionItem={actionItems[i]}/></section>))}
+      </div>
+      <Footer color="dark_blue"/>
     </>
-  )
+  );
 }
