e712c19d6047f3ea6f5ca97b4a49f3e265dae8ae
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-12-30 19:41:07 +0000

    :art: Update design

--- pages/profile.js
+++ pages/profile.js
@@ -52,15 +52,10 @@ export default function Profile() {
               />
               <a href="#" className="pl-6 text-aqua h-auto inline-block underline">Reset Password</a>
 
-              <FileInput
-                  text="UPLOAD YOUR CV"
-                  id="cv"
-                  name="cv"
-              />
           </Form>
         </div>
 
-        <div className="col-span-1 w-full md:w-1/2 float-right md:pl-6">
+        <div className="mt-10 md:mt-0 col-span-1 w-full md:w-1/2 float-right md:pl-6">
           <div>
             <Heading text="Achievements"></Heading>
 
@@ -94,6 +89,33 @@ export default function Profile() {
 
             <p className="font-iregular">You just need 4 more bages to go to Level 4 (and win 10+ entries to the final draw). Hurry!</p>
             <CodeInput/>
+          </div>
+
+          <div className="mt-10">
+            <Heading text="Upload CV"></Heading> 
+            <p className="font-iregular"> Get a chance to win a spot at the Coporate dinner by submiting you CV!</p>
+
+            <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:pt-5">
+              <div class="mt-1 sm:mt-0 sm:col-span-2">
+                <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                  <div class="space-y-1 text-center">
+                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                      <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <div class="flex text-sm text-gray-600">
+                      <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-primary hover:text-primary">
+                        <span>Upload a file</span>
+                        <input id="file-upload" name="file-upload" type="file" class="sr-only"/>
+                      </label>
+                      <p class="pl-1">or drag and drop</p>
+                    </div>
+                    <p class="text-xs text-gray-500">
+                      PDF
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
