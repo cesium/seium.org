251a861121b1bbd77dddd28c0a6e78fcccb96c9b
Rui Oliveira <rpoliveira02@gmail.com>
2021-12-30 19:16:10 +0000

    Changed footer text

--- pages/challenges.js
+++ pages/challenges.js
@@ -3,12 +3,18 @@ import Challenges from '/components/website/challenges/Challenges';
 import Footer from '/components/website/utils/Footer';
 
 
+function FooterText() {
+  return (
+    <>Just really useful links here. Bye now 👋</>
+  );
+}
+
 export default function Index() {
   return (
     <>
       <Hero />
       <Challenges />
-      <Footer color="secondary" />
+      <Footer color="secondary" animationText={<FooterText/>}/>
     </>
   );
 }
