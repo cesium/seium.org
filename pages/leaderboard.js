8f73e4ef700e34b9d927a98e9bb42e4d77083b3b
Rui Oliveira <rpoliveira02@gmail.com>
2021-12-31 11:15:47 +0000

    Added leaderboard table

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import Dashboard from "/components/moonstone/user/utils/Dashboard";
-import Schedule from '/components/website/home/Schedule';
+import LeaderboardTable from "/components/moonstone/user/leaderboard/LeaderboardTable";
 
 export default function Leaderboard() {
     const defaultState = {
@@ -17,6 +17,19 @@ export default function Leaderboard() {
             date: st.date,
         });
     }
+
+    const testUser = {
+        name: "John Doe",
+        badges: 42,
+    }
+
+    const me = {
+        name: "Me",
+        badges: 0
+    }
+
+    const list = [testUser, testUser, testUser, testUser, testUser, testUser, testUser, testUser, me];
+
     return (
         <Dashboard href="leaderboard" title="Leaderboard" description="Check the users with the highest number of badges">
             <div className="grid-cols-2 overflow-hidden mt-12">
@@ -26,9 +39,11 @@ export default function Leaderboard() {
                 <div className="col-span-1 w-full md:w-1/2 float-left">
                     <b className="text-ibold">Board</b>
                     <button className={`font-iregular bg-${st.hallOfFame ? "white" : "quinary"} rounded-full h-12 ml-24 text-center items-center px-4 py-1`}
-                        onClick={(e) => changeLeaderboard(false)}>Leaderboard</button>
+                        onClick={(e) => changeLeaderboard(false)}>LEADERBOARD</button>
                     <button className={`font-iregular bg-${st.hallOfFame ? "quinary" : "white"} rounded-full h-12 ml-12 text-center items-center px-4 py-1`}
-                        onClick={(e) => changeLeaderboard(true)}>Hall Of Fame</button>
+                        onClick={(e) => changeLeaderboard(true)}>HALL OF FAME</button>
+
+                    <LeaderboardTable list={list} user="Me" maxUsersToShow={5}/>
                 </div>
             </div>
         </Dashboard>
