27ffbb6159f8f3f1d702a8314db9efe737bf9f32
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-12-31 07:12:15 +0000

    Update social

--- components/website/utils/Social/index.jsx
+++ components/website/utils/Social/index.jsx
@@ -4,20 +4,20 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Social() {
   return (
-      <div className="mt-4 grid justify-items-center grid-cols-5 gap-x-2">
-        <a className="w-4" target="_blank" href="https://www.facebook.com/SEI.UMinho/">
+      <div className="mt-4 grid justify-items-center grid-cols-5 gap-x-6">
+        <a className="w-5 opacity-50 hover:opacity-100" target="_blank" href="https://www.facebook.com/SEI.UMinho/">
           <FontAwesomeIcon icon={faGithubAlt} />
         </a>
-        <a className="w-4" target="_blank" href="https://www.facebook.com/SEI.UMinho/">
+        <a className="w-5 opacity-50 hover:opacity-100" target="_blank" href="https://www.facebook.com/SEI.UMinho/">
           <FontAwesomeIcon icon={faFacebook} />
         </a>
-        <a className="w-4" target="_blank" href="https://www.instagram.com/sei.uminho">
+        <a className="w-5 opacity-50 hover:opacity-100" target="_blank" href="https://www.instagram.com/sei.uminho">
           <FontAwesomeIcon icon={faInstagram} />
         </a>
-        <a className="w-4" target="_blank" href="https://www.facebook.com/SEI.UMinho/">
+        <a className="w-5 opacity-50 hover:opacity-100" target="_blank" href="https://www.facebook.com/SEI.UMinho/">
           <FontAwesomeIcon icon={faLinkedinIn} />
         </a>
-        <a className="pt-0.5 w-4" target="_blank" href="mailto:geral@seium.org">
+        <a className="pt-0.5 w-5 opacity-50 hover:opacity-100" target="_blank" href="mailto:geral@seium.org">
           <FontAwesomeIcon icon={faEnvelope} />
         </a>
       </div>
