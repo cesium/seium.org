0318c6f4f8b1bf66dc3fb0e3f9e966fb42a7d9df
feliciofilipe <filipe.felicio.1999@gmail.com>
2021-12-31 03:11:08 +0000

    Clean default branch

--- src/components/moonstone/latestWins/Winner.js
+++
@@ -1,9 +0,0 @@
-export default function Winner(props) {
-  return (
-    <div className="winner">
-      <p className="winnerName info">{props.user}</p>
-      <p className="award info">{props.award}</p>
-      <p className="timing info">{props.time}</p>
-    </div>
-  );
-}
--- src/components/moonstone/owners/Owner.js
+++
@@ -1,7 +0,0 @@
-export default function Owner(props) {
-  return (
-    <div className="owner">
-      <p className="owner-name">{props.user}</p>
-    </div>
-  );
-}
