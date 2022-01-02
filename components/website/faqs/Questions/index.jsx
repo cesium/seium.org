275ce305d7fc87709672eab46f10de4f69150a3b
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-01-02 06:21:05 +0000

    Fix key warning on faqs

--- components/website/faqs/Questions/index.jsx
+++ components/website/faqs/Questions/index.jsx
@@ -1,33 +1,12 @@
-import Link from 'next/link'
-import React, { useState } from 'react';
+import Question from './Question';
 
 import questions from '/data/faqs';
-
-function Question(props) {
-    const { title, description, href, link } = props
-    const [showQuestion, setShowQuestion] = useState(true)
-
-    return (
-        <div className="text-white border-t-2 border-white py-4">
-            <h2 className="text-2xl md:text-4xl font-iextrabold mb-4">{title}</h2>
-            <p className={showQuestion ? "hidden" : ""}>{description}</p>
-            <div className="flex justify-end items-center">
-                <Link href={href}>
-                    <a className="text-quinary font-iextrabold text-sm mr-4">{link}</a>
-                </Link>
-                <button className="bg-quinary px-2 font-iextrabold text-xl text-white rounded-full w-16" onClick={() => setShowQuestion(!showQuestion)}>
-                    {showQuestion ? "+" : "-"}
-                </button>
-            </div>
-        </div>
-    );
-}
 
 export default function Questions(props) {
     return (
         <div className="flex flex-col">
-            {questions.map((question) => (
-                <Question {...question} />
+            {questions.map((question,i) => (
+                <Question key={i} {...question} />
             ))}
         </div>
     );
