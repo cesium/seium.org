704683d1eb5bfa89e4d8120aba0a5882822b8591
Rui Oliveira <rpoliveira02@gmailcom>
2022-01-04 15:08:53 +0000

    Added wheel

--- pages/wheel.js
+++ pages/wheel.js
@@ -1,64 +1,56 @@
-import Image from 'next/image'
+import {useState, useEffect} from 'react';
 
 import Dashboard from "/components/moonstone/Dashboard";
-import Form from '/components/moonstone/Form';
-import Input from '/components/moonstone/Input';
 import Heading from '/components/moonstone/Heading';
-import Button from '/components/utils/Button';
-import CodeInput from '/components/moonstone/CodeInput';
-import CheckpointTracker from '/components/moonstone/CheckpointTracker';
 import ListItem3 from '../components/moonstone/ListItem3Cols';
 import ListItem4 from '../components/moonstone/ListItem4Cols';
+import Wheel from '/components/moonstone/Wheel';
 
 export default function Profile() {
+  const defaultState = {
+    angle: 0,
+    speed: 20,
+  }
+  const angleSpeed = 20;
+  const fps = 60;
+  const [st, updateState] = useState(defaultState);
+
+  const changeState = () => {
+    updateState({
+      angle: (st.angle + st.speed) % 360,
+      speed: st.speed - 0.1,
+    });
+  }
+
+  //Rotate at 60fps
+  useEffect(() => {
+    console.log(st.angle);
+    if(st.speed > 0)
+      setTimeout(changeState, 1000 / 60);
+  }, [st]);
+
   return (
     <Dashboard href="sweel" title="Wheel" description="Spin the wheel and win awards!">
-      <div className="grid-cols-2 overflow-hidden">
-        <div className="col-span-1 w-full md:w-1/2 float-left">
+      <div className="grid-cols-1 2xl:grid-cols-2 overflow-hidden">
+        <div className="col-span-1 w-full 2xl:w-1/2 float-left">
           <Heading text="Achievements">
-            <div className="w-40 h-full">
+            <div className="w-40 h-full pt-1">
                 <div className="col-span-1 float-left w-full">
                 💰170 Tokens
                 </div>
             </div>
-            <div className="w-40 h-full">
+            <div className="w-40 h-full pt-1">
                 <div className="col-span-1 float-left w-full">
                 🏅68 Badges
                 </div>
             </div>
           </Heading>
-          <div className="pl-6">          
-            <Image src="/images/speakers/joaooliveira.png" className="rounded-full overflow-hidden" width="220" height="220" />
+          <div className="w-96 h-96 m-auto">
+            <Wheel steps={16} angle={st.angle}/>
           </div>
-          <Form>
-              <Input
-                  text="NAME"
-                  id="name"
-                  name="name"
-                  bgColor="white"
-                  fgColor="black"
-              />
-              <Input
-                  text="USERNAME"
-                  id="username"
-                  name="username"
-                  bgColor="white"
-                  fgColor="black"
-              />
-              <Input
-                  text="PASSWORD"
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  bgColor="white"
-                  fgColor="black"
-              />
-              <a href="#" className="pl-6 text-aqua h-auto inline-block underline">Reset Password</a>
-          </Form>
         </div>
 
-        <div className="col-span-1 w-full md:w-1/2 float-right md:pl-6">
+        <div className="col-span-1 w-full 2xl:w-1/2 float-right 2xl:pl-6">
           <div>
             <Heading text="Latest Wins"></Heading>
             <div className="h-72">
@@ -73,7 +65,7 @@ export default function Profile() {
 
           <div className="mt-10">
             <Heading text="Checkpoints"></Heading>
-              <div className={`w-full mb-5 pb-3 grid grid-cols-4`}>
+              <div className="w-full mb-5 pb-3 grid grid-cols-4">
                 <div className="text-left">
                     <p className="font-iregular">Name</p>
                 </div>
