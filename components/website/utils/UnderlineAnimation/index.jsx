993837d34fbe6d5da8c3e8e374ce314681462e1c
Rui Oliveira <rpoliveira02@gmailcom>
2022-01-06 13:25:04 +0000

    Improved mascot animation

--- components/website/utils/UnderlineAnimation/index.jsx
+++ components/website/utils/UnderlineAnimation/index.jsx
@@ -1,28 +1,72 @@
-import {useState } from 'react';
-import Fade from "react-reveal/Fade";
+import { useState, useEffect } from 'react';
 
 import Card from "/components/utils/Card";
 
 import styles from './style.module.css';
 
-export default function UnderlineAnimation({children, text}) {
-    const [hovered, setHover] = useState(false);
-    return (
-        <span className="relative leading-none z-10 w-auto my-4 inline-block">
-                <h2 className="font-bold text-white md:w-full relative z-0 border-b-8 border-b-solid border-white">
-                    <span className="relative h-auto inline-block z-0" onMouseEnter={() => setHover(true)} 
-                        onMouseLeave={() => setHover(false)}>
-                            {children}
-                    </span>
-                </h2>
+export default function UnderlineAnimation({children, text, afterText}) {
+    const extendedMargin = -180;
+    const retractedMargin = -20;
+    const speed = 4;
+    const fps = 60;
 
-                <div className={styles.cardWrapper}>
-                    <Fade top when={hovered} distance="10px">
-                        <Card className="z-0" img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
-                            <h5 className={`font-iregular font-normal ${styles.cardText}`}>{text}</h5>
-                        </Card>
-                    </Fade> 
-                </div>
+    const defaultState = {
+        status: 0,
+        margin: extendedMargin,
+    };
+    
+    const [st, updateState] = useState(defaultState);
+
+    const scrollDown = () => {
+        if(st.margin == retractedMargin) {
+            updateState({
+                status: 0,
+                margin: retractedMargin,
+            });
+        } else {
+            updateState({
+                status: st.status,
+                margin: st.margin + speed,
+            });
+        }
+    }
+
+    const scrollUp = () => {
+        if(st.margin == extendedMargin) {
+            updateState({
+                status: 0,
+                margin: extendedMargin,
+            });
+        } else {
+            updateState({
+                status: st.status,
+                margin: st.margin - speed,
+            });
+        }
+    }
+
+    useEffect(() => {
+        if(st.status == 1) {
+            setTimeout(scrollDown, 1000 / fps);
+        } else if(st.status == 3) {
+            setTimeout(scrollUp, 1000 / fps);
+        }        
+      }, [st]);
+
+    return (
+        <span className="relative leading-none z-10 w-full inline-block z-20 mt-4">
+            <div className="absolute bottom-0 w-full h-48 bg-secondary z-20" style={{width: "500px"}}></div>
+            <span className="relative h-full inline-block z-20 border-b-4 border-white" onMouseEnter={() => updateState({status:1, margin:st.margin})} 
+                onMouseLeave={() => updateState({status:3, margin: st.margin})}>
+                    {children}
             </span>
+            <span className="relative h-auto inline-block z-20">&nbsp;{afterText ? afterText : ""}</span>
+            <div className="bg-secondary absolute top-0 left-0 w-full h-full z-10 -my-1"></div>
+            <div className="absolute top-full left-20 pt-3 z-0" style={{marginTop: st.margin + "px"}}>      
+                <Card img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
+                    <h5 className={`font-ithin ${styles.cardText}`}>{text}</h5>
+                </Card>
+            </div>
+        </span>
     );
 }
