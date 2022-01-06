b2235ae601d4e30d3c9305a0ecfa2d2ca737ab0d
Rui Oliveira <rpoliveira02@gmailcom>
2022-01-06 22:22:47 +0000

    Changed way card is hidden to overflow on absolute div

--- components/website/utils/UnderlineAnimation/index.jsx
+++ components/website/utils/UnderlineAnimation/index.jsx
@@ -5,8 +5,8 @@ import Card from "/components/utils/Card";
 import styles from './style.module.css';
 
 export default function UnderlineAnimation({children, text, afterText}) {
-    const extendedMargin = -220;
-    const retractedMargin = -20;
+    const extendedMargin = -240;
+    const retractedMargin = -12;
     const speed = 4;
     const fps = 60;
 
@@ -51,22 +51,24 @@ export default function UnderlineAnimation({children, text, afterText}) {
         } else if(st.status == 3) {
             setTimeout(scrollUp, 1000 / fps);
         }        
-      }, [st]);
+    }, [st]);
 
     return (
-        <span className="relative leading-none z-10 w-full inline-block z-20 mt-4">
-            <div className="absolute bottom-0 w-full h-52 bg-secondary z-20" style={{width: "500px"}}></div>
-            <span className="relative h-full inline-block z-20 border-b-8 border-white" onMouseEnter={() => updateState({status:1, margin:st.margin})} 
-                onMouseLeave={() => updateState({status:3, margin: st.margin})}>
+        <span className="relative leading-none z-10 w-full h-auto inline-block z-10 mt-4">
+            
+            <span className="relative h-auto inline-block z-10 border-b-8 border-white" onMouseEnter={() => updateState((oldState) => ({...oldState, status: 1}))} 
+                onMouseLeave={() => updateState((oldState) => ({ ...oldState, status: 3}))}>
                     {children}
             </span>
-            <span className="relative h-auto inline-block z-20">&nbsp;{afterText ? afterText : ""}</span>
-            <div className="bg-secondary absolute top-0 left-0 w-full h-full z-10 -my-1"></div>
-            <div className="absolute top-full left-20 pt-3 z-0" style={{marginTop: `${st.margin}px`}}>      
+            <span className="relative h-auto inline-block z-10">&nbsp;{afterText ? afterText : ""}</span>
+            <div className="absolute top-full  w-full h-52 z-10 overflow-hidden">
+            <div className="absolute top-0 left-20 pt-3 z-0" style={{marginTop: `${st.margin}px`}}>      
                 <Card img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
                     <h5 className={`font-ithin ${styles.cardText}`}>{text}</h5>
                 </Card>
             </div>
+            </div>
+            
         </span>
     );
 }
