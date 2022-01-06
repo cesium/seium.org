7967471937d11d5d972cb3b717b409681089ed73
Rui Oliveira <rpoliveira02@gmailcom>
2022-01-06 13:28:25 +0000

    Fixed animation in challenges page

--- components/website/challenges/Hero/Title/index.jsx
+++ components/website/challenges/Hero/Title/index.jsx
@@ -8,7 +8,8 @@ export default function Title() {
                 Challenges
             </h5>
             <h1 className="font-bold text-white text-6xl w-11/12 md:text-8xl md:w-full xl:text-9xl 2xl:w-4/5 relative z-0">
-                Participate in new challenges every <UnderlineAnimation text="You can also win awards every day. But that's highly unlikely, I would say....">day</UnderlineAnimation>     
+                <span className="relative z-50">Participate in new challenges every </span>
+                <UnderlineAnimation text="You can also win awards every day. But that's highly unlikely, I would say....">day</UnderlineAnimation>     
             </h1>     
         </div>
     );
