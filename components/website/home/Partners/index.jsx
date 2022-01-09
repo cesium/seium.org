f201d52a4e864f377339f0ec5482ff545fd412d2
Rui Oliveira <rpoliveira02@gmailcom>
2022-01-09 21:38:27 +0000

    Added partners

---
+++ components/website/home/Partners/index.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+import partners from "/data/partners";
+
+export default function Partners() {
+    
+    const maxPerRow = 4;
+    let rows = [];
+    let cur = 0;
+    console.log(partners.length);
+    while(cur <= partners.length) {
+        let noElements = Math.min(partners.length - cur, maxPerRow);
+        console.log(cur);
+        rows.push(  <div className={`spacing grid grid-cols-${noElements} gap-${noElements} mb-10`}>
+                        {[...Array(noElements).keys()].map((entry, i) => 
+                            <div className="m-auto" key={entry}>
+                                <Image src={`/images/partners/${partners[entry].image}`} width={partners[i].width} height={partners[i].height}/>
+                            </div>)}
+                    </div>);
+        cur += maxPerRow;
+    }
+
+    return (
+        <div className="bg-secondary w-full pt-10 pb-10">
+            <h2 className="text-6xl font-iextrabold py-10 px-6 flex justify-center text-white">Partners who made this possible</h2>
+            {rows}
+        </div>
+    );
+}
+/*
+ * <div className="spacing grid grid-cols-4 gap-4">
+                <div className="align-center">
+                    <Image src="/images/partners/um-school-of-engineering.png" width="300" height="272"/>
+                </div>
+                <div>
+                    <Image src="/images/partners/um-school-of-engineering.png" width="300" height="272"/>
+                </div>
+                <div>
+                    <Image src="/images/partners/um-school-of-engineering.png" width="300" height="272"/>
+                </div>
+                <div>
+                    <Image src="/images/partners/um-school-of-engineering.png" width="300" height="272"/>
+                </div>
+            </div>       
+ */
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Hero from '/components/website/home/Hero';
 import Sponsors from '/components/website/home/Sponsors';
 import Hackathon from '/components/website/home/Hackathon';
 import Speakers from '/components/website/home/Speakers';
+import Partners from "/components/website/home/Partners";
 
 import Schedule from '/components/website/utils/Schedule';
 import Footer from '/components/website/utils/Footer';
@@ -12,6 +13,7 @@ export default function Index() {
       <Hero />
       <Schedule color="tertiary" detailed={false}/>
       <Sponsors/>
+      <Partners/>
       <Hackathon />
       <Speakers />
       <Footer color="secondary" />
