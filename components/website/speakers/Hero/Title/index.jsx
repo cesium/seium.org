1afddf681e6587c6e40328be77026845c9fa8df5
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-01-10 10:35:57 +0000

    Make speakers page responsive

--- components/website/speakers/Hero/Title/index.jsx
+++ components/website/speakers/Hero/Title/index.jsx
@@ -4,7 +4,7 @@ export default function Title() {
             <h5 className="text-2xl text-quinary">
                 Speakers
             </h5>
-            <h1 className="text-white text-6xl lg:text-8xl 2xl:text-9xl md:w-4/5 relative z-0">
+            <h1 className="text-white text-7xl lg:text-8xl 2xl:text-9xl md:w-4/5 relative z-0">
                 A crazy cool selection of speakers.
             </h1>
         </div>
