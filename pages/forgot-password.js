b63b5ed392ae9c036ae05f41fbbeba09b9a2aaaa
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2022-01-10 15:44:38 +0000

    Added pages to reset password (#172)

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -3,6 +3,8 @@ import Fade from "react-reveal/Fade";
 import Button from "/components/utils/Button";
 import Card from "/components/utils/Card";
 
+import { useState } from "react";
+
 import Return from "/components/moonstone/utils/Return";
 import Form from "/components/moonstone/utils/Form";
 import Input from "/components/moonstone/utils/Input";
@@ -10,12 +12,22 @@ import Input from "/components/moonstone/utils/Input";
 import Title from "/components/moonstone/authentication/Title";
 import Text from "/components/moonstone/authentication/Text";
 
-export default function Login() {
+export default function ForgotPassword() {
+    /*
+    
+    Null  -> No email sent
+    True  -> Email sent successfully
+    False -> Error occured sending email
+
+    */
+    const [st, updateState] = useState(null);
+
     return (
         <div className="overflow-hidden bg-secondary min-h-screen">
             <Return ml="4" mt="10" mt_sm="10" />
             <div className="mt-10 sm:mt-40 flex flex-col items-center justify-center">
-                <Title text="Log in" />
+                <Title text="Reset password" />
+                {st != true ? 
                 <div className="mt-8">
                     <Form>
                         <Input
@@ -27,24 +39,26 @@ export default function Login() {
                             bgColor="primary"
                             autoComplete="email"
                         />
-                        <Input
-                            text="YOUR PASSWORD"
-                            id="password"
-                            name="password"
-                            type="password"
-                            fgColor="white"
-                            bgColor="primary"
-                            autoComplete="current-password"
-                        />
-                        <Text padding="6" text="Forgot your password?" link="Right this way" href="/forgot-password" />
                         <Button
                             type="submit"
                             text="LET'S GO"
-                            customStyle="text-secondary bg-quinary border-quinary"
+                            fg_color="black"
+                            bg_color="quinary"
+                            border="quinary"
+                            padding="32"
                         />
                     </Form>
-                </div>
-                <Text text="Don’t have an account?" link="Signup here" href="/signup" />
+                </div> : <></>}
+                {st == false ?
+                <p className="mt-10 font-iregular text-red-600">An error has occured. Please make sure the email you provided is
+                    correct and try again later
+                </p> : <></>}
+
+                {st == true ?
+                <p className="mt-10 font-iregular text-quinary">An email has bent sent. Please check your inbox to recover your password
+                </p> : <></>}
+
+                {st != true ? <Text text="Don’t have an account?" link="Signup here" href="/signup" /> : <></> }
                 <div className="hidden lg:block absolute bottom-0 right-60">
                     <Fade bottom>
                         <Card img="/images/mascot-footer.svg" alt="MascotFooter" inverted={false}>
