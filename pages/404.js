7cf813e274fae968805bb05e04e88ff855dafb5c
ruioliveira02 <rpoliveira02@gmailcom>
2022-01-10 09:59:20 +0000

    Fixed background

--- pages/404.js
+++ pages/404.js
@@ -5,12 +5,14 @@ import Button from '/components/utils/Button'
 
 export default function Error() {
     return (
-        <div className="spacing h-screen bg-secondary">
-            <div className="absolute invisible xl:visible xl:w-full xl:h-screen left-0">
+        <>
+        <div className="absolute w-screen h-screen bg-secondary z-0"></div>
+        <div className="spacing bg-secondary relative z-10">
+            <div className="absolute secondary invisible xl:visible xl:w-full xl:h-screen left-0">
                 {<Image src="/images/ponto.svg" layout="fill" alt="Question mark" />}
             </div>
             <Navbar button="quinary" fg_color="black" />
-            <div className="pt-14">
+            <div className="pt-14 bg-secondary">
                 <div className="font-iextrabold z-50 relative">
                     <h5 className="text-7xl lg:text-8xl xl:text-9xl text-quinary">
                         404
@@ -23,9 +25,10 @@ export default function Error() {
                     <Button onClick={(e) => window.location.href = "/"} text="Back to Homepage" customStyle="text-white bg-primary border-tertiary hover:bg-tertiary"/>
                 </div>
             </div>
-            <div className="absolute invisible bg-secondary xl:visible xl:w-1/4 xl:h-1/2 bottom-0 right-0">
+            <div className="fixed invisible bg-secondary xl:visible w-1/4 h-1/2 bottom-0 right-0">
                 <Image src="/images/mascote-404.svg" layout="fill" alt="Mascote Image" />
             </div>
         </div>
+        </>
     )
 }
