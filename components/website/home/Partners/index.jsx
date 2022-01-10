ed8172012d26d1023bb2ef296bc4e85325ed29d0
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-01-10 15:14:43 +0000

    Update design

--- components/website/home/Partners/index.jsx
+++ components/website/home/Partners/index.jsx
@@ -1,32 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+
 import partners from "/data/partners";
 
 export default function Partners() {
-    
-    const maxPerRow = 4;
-    let rows = [];
-    let cur = 0;
-
-    while(cur <= partners.length) {
-        let noElements = Math.min(partners.length - cur, maxPerRow);
-        rows.push(  <div className={`spacing grid grid-cols-2 lg:grid-cols-${noElements} gap-${noElements} mb-10`}>
-                        {[...Array(noElements).keys()].map((entry, i) => 
-                            <div className="m-auto w-3/4 lg:w-1/4 filter grayscale" key={entry}>
-                                <Link href={partners[i].url}>
-                                    <a className="w-full h-full" target="_blank">
-                                        <Image src={`/images/partners/${partners[entry].image}`} layout="responsive" width={partners[i].width} height={partners[i].height} />
-                                    </a>
-                                </Link>      
-                            </div>)}
-                    </div>);
-        cur += maxPerRow;
-    }
-
     return (
-        <div className="bg-secondary w-full pt-10 pb-10">
-            <h2 className="text-6xl font-iextrabold py-10 px-6 flex justify-center text-white">Partners who made this possible</h2>
-            {rows}
+        <div className="spacing bg-secondary w-full pt-10 pb-10">
+            <h2 className="text-center text-5xl lg:text-6xl font-iextrabold py-10 text-white">Partners who made this possible</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-10 my-10">
+                {partners.map((partner, i) =>
+                    <div className={`col-span-${4 / partners.length} m-auto filter grayscale`} key={i}>
+                        <Link href={partner.url}>
+                            <button>
+                                <Image src={`/images/partners/${partner.image}`} width="200" height="200" />
+                            </button>
+                        </Link>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
