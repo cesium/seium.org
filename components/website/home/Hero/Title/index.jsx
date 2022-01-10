8ebfa545a10004c3df1e52d67337667eb18cfee1
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2022-01-10 10:11:08 +0000

    Improved mascot animation (#159)

--- components/website/home/Hero/Title/index.jsx
+++ components/website/home/Hero/Title/index.jsx
@@ -1,33 +1,14 @@
-import {useState } from 'react';
-import Fade from "react-reveal/Fade";
-
-import Card from "/components/utils/Card";
-
-import styles from './style.module.css';
+import UnderlineAnimation from "/components/website/utils/UnderlineAnimation";
 
 export default function Title() {
-    const [hovered, setHover] = useState(false);
     return (
-        <div className="font-bold z-50 relative">
+        <div className="font-bold z-20 relative">
             <h5 className="font-iextrabold text-2xl text-quinary m-1">
                 15-20 February 2022
             </h5>
-            <h1 className="font-iextrabold text-white text-6xl w-11/12 md:text-8xl md:w-full xl:text-9xl 2xl:w-4/5 relative z-0">
-                The software engineering week is back, let's just&nbsp;
-                <span className="relative leading-none z-10 w-auto my-4 inline-block">
-                    <u className="relative h-auto inline-block z-0" onMouseEnter={() => setHover(true)} 
-                        onMouseLeave={() => setHover(false)}>
-                            SEI
-                    </u>
-                    <span className="relative h-auto inline-block z-10"> &nbsp;that</span>
-                    <div className={styles.cardWrapper}>
-                        <Fade top when={hovered} distance="10px">
-                            <Card className="z-0" img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
-                                <h5 className={`font-ithin ${styles.cardText}`}>Did you see what I did there?</h5>
-                            </Card>
-                        </Fade> 
-                    </div>
-                </span>
+            <h1 className="relative font-iextrabold text-white text-6xl w-11/12 md:text-8xl md:w-full xl:text-9xl 2xl:w-4/5 relative z-20">
+                <span className="relative z-20">The software engineering week is back, let's just&nbsp;</span>
+                <UnderlineAnimation text="Did you see what I did there?" afterText="that">SEI</UnderlineAnimation>
             </h1>
         </div>
     );
