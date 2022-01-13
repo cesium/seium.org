e9c097f9dbf7e7c1aa08361738db32f9497d8067
rubenadao <rubenadao101@gmail.com>
2022-01-13 17:01:48 +0000

    New Animations added

--- components/website/schedule/Hero/index.jsx
+++ components/website/schedule/Hero/index.jsx
@@ -4,10 +4,12 @@ import Navbar from '/components/website/utils/Navbar';
 
 import Title from "./Title";
 
+const Animation = dynamic(() => import('../Animation'), { ssr: false })
 export default function Hero() {
   return (
     <div className=" bg-tertiary">
       <div className="spacing pb-32 z-50 relative">
+      <Animation />
         <Navbar button="quinary" fg_color="black"/>
         <div className="pt-2">
           <Title />
