80fabf80c4965e90bcfe3d7b06bfa4534e11366b
Maria <mariajbp00@gmail.com>
2022-01-14 15:43:27 +0000

    remove lorem

--- components/website/faqs/index.jsx
+++ components/website/faqs/index.jsx
@@ -4,17 +4,13 @@ import questions from "/data/faqs";
 
 export default function Faqs() {
   return (
-    <div className="spacing py-20 flex flex-col lg:flex-row justify-between bg-secondary lg:h-3/4">
-      <div className="text-white lg:w-2/5 mb-10">
-        <h1 className="text-4xl lg:text-6xl font-bold mb-4 font-iextrabold">
+    <div className="flex flex-col justify-between py-20 spacing lg:flex-row bg-secondary lg:h-3/4">
+      <div className="mb-10 text-white lg:w-2/5">
+        <h1 className="mb-4 text-4xl font-bold lg:text-6xl font-iextrabold">
           Frequently Asked Questions
         </h1>
         <p className="text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          mattis erat in tellus porta, ut suscipit arcu iaculis. Sed tempor
-          massa ipsum, et ornare dui dignissim vitae. Maecenas erat felis,
-          laoreet sit amet felis quis, molestie pellentesque purus. Nam a
-          euismod ante, a tincidunt elit.
+          
         </p>
       </div>
       <div className="lg:w-1/3">
