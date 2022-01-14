8a5214e02d803a0b3a0c4ec5edcb19f3a109198f
Maria <mariajbp00@gmail.com>
2022-01-14 15:58:37 +0000

    Update navbar links

--- components/website/utils/Navbar/index.jsx
+++ components/website/utils/Navbar/index.jsx
@@ -3,25 +3,24 @@ import Image from "next/image";
 const navigation = [
     { name: 'Schedule', href: '/schedule' },
     { name: 'Hackathon', href: '/hackathon' },
-    { name: 'Team', href: '/team' },
     { name: 'Challenges', href: '/challenges' },
     { name: 'Speakers', href: '/speakers' },
     { name: 'FAQs', href: '/faq' },
-    { name: 'Moonstone', href: '/login' },
+    { name: 'Login', href: '/login' },
 ]
 
 export default function Navbar({ button, fg_color }) {
     return (
-        <div className="pt-8 flex flex-auto z-50 relative">
-            <div className="grid grid-cols-4 w-full">
+        <div className="relative z-50 flex flex-auto pt-8">
+            <div className="grid w-full grid-cols-4">
                 <a href="/" className="">
                     <Image src="/images/sei-logo.svg" width="50" height="40" />
                 </a>
-                <div className="justify-self-end col-span-3 invisible xl:visible flex flex-auto gap-x-20">
+                <div className="flex flex-auto invisible col-span-3 justify-self-end xl:visible gap-x-20">
                     <div className="grid grid-cols-4 gap-x-10">
                         {navigation.map((item, i) => (
                             <a key={i}
-                                className="font-iregular text-white text-opacity-40 hover:text-opacity-100"
+                                className="text-white font-iregular text-opacity-40 hover:text-opacity-100"
                                 href={item.href}
                             >
                                 {item.name}
