c0de8fda02affa6c9501cc86759f174d9f2f51a5
Filipe Felício <55213469+feliciofilipe@users.noreply.github.com>
2022-01-14 09:40:25 +0000

    Update index.jsx

--- components/website/home/Sponsors/Tab/index.jsx
+++ components/website/home/Sponsors/Tab/index.jsx
@@ -2,9 +2,9 @@ import styles from './style.module.css';
 
 export default function SponsorTab({tabName, selected, onSelect}) {
     return (
-        <div className={styles.wrapper}  onClick={onSelect}>
+        <button className={styles.wrapper}  onClick={onSelect}>
             <span className={`font-imedium ${styles.text}`}>{tabName}</span>
             <button className={selected ? styles.button_selected : styles.button_no_selected} />
-        </div>
+        </button>
     );
 }
