ebaeb7a3b02503abd18e0457dd5e1a230c92316f
Maria <32769382+mariajbp@users.noreply.github.com>
2022-01-15 16:17:01 +0000

    change mascot messsage on schedule (#226)

--- components/website/schedule/Hero/Title/index.jsx
+++ components/website/schedule/Hero/Title/index.jsx
@@ -2,13 +2,13 @@ import UnderlineAnimation from '/components/website/utils/UnderlineAnimation';
 
 export default function Title() {
     return (
-        <div className="font-bold z-50 relative">
-            <h5 className="font-iextrabold text-2xl text-quinary m-1">
+        <div className="relative z-50 font-bold">
+            <h5 className="m-1 text-2xl font-iextrabold text-quinary">
                 Schedule
             </h5>
-            <h1 className="font-iextrabold text-white text-6xl w-11/12 md:text-8xl md:w-full xl:text-9xl 2xl:w-4/5 relative z-0">
-                Five awesome days of learning, sharing and &nbsp;
-                <UnderlineAnimation text="Did you see what I did there?" afterText="">winning</UnderlineAnimation>
+            <h1 className="relative z-0 w-11/12 text-6xl text-white font-iextrabold md:text-8xl md:w-full xl:text-9xl 2xl:w-4/5">
+                Five awesome days of learning, sharing and
+                <UnderlineAnimation text="Or maybe losing. This one is kind of optional." afterText="">winning</UnderlineAnimation>
             </h1>
         </div>
     );
