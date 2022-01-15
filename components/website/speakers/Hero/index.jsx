a4d7b9d6a7280bb47ce65c3abcab8fc6c3e48dba
Ruben Adão <42103120+rubenadao@users.noreply.github.com>
2022-01-15 14:40:16 +0000

    New Animations added (#200)

--- components/website/speakers/Hero/index.jsx
+++ components/website/speakers/Hero/index.jsx
@@ -2,10 +2,13 @@ import dynamic from 'next/dynamic'
 
 import Title from "./Title";
 
+const Animation = dynamic(() => import('./Animation'), { ssr: false })
+
 export default function Hero() {
   return (
     <div className="bg-secondary spacing pb-40">
       <div className="z-50 relative">
+        <Animation />
         <div className="pt-2">
           <Title />
         </div>
