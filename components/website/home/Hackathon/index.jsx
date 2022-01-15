97a925b2929d44f4684108c18102f1db1dec460e
Maria <32769382+mariajbp@users.noreply.github.com>
2022-01-15 15:17:48 +0000

    add hackathon link and remove team button (#221)

--- components/website/home/Hackathon/index.jsx
+++ components/website/home/Hackathon/index.jsx
@@ -1,26 +1,31 @@
 import Button from "/components/utils/Button";
 
 export default function Hackathon() {
-    return (
-        <div className="spacing bg-tertiary py-20">
-            <div className="max-w-screen-xl mx-auto">
-                <div className="px-5">
-                    <p className="text-2xl text-quinary font-iextrabold">
-                        Hackathon 2022
-                    </p>
-                    <p className="font-iextrabold text-5xl md:text-6xl lg:text-8xl text-white">
-                        Create products, train skills and learn new technologies
-                    </p>
-                    <div className="sm:flex pt-6 sm:pt-4">
-                        <span className="self-center w-56">
-                            <Button text="REGISTER YOUR TEAM" customStyle="text-white bg-secondary border-gray-900 hover:bg-gray-900" />
-                        </span>
-                        <div className="pt-3 sm:pt-0 self-center pl-4 sm:pl-8 opacity-80 text-white text-md">
-                            2-5 people
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="py-20 spacing bg-tertiary">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="px-5">
+          <p className="text-2xl text-quinary font-iextrabold">
+            Hackathon 2022
+          </p>
+          <p className="text-5xl text-white font-iextrabold md:text-6xl lg:text-8xl">
+            Create products, train skills and learn new technologies
+          </p>
+          <div className="pt-6 sm:flex sm:pt-4">
+            <span className="self-center w-56">
+              <a href="https://forms.gle/8aSEUubkjei1Dpym6">
+                <Button
+                  text="REGISTER YOUR TEAM"
+                  customStyle="text-white bg-secondary border-gray-900 hover:bg-gray-900"
+                />
+              </a>{" "}
+            </span>
+            <div className="self-center pt-3 pl-4 text-white sm:pt-0 sm:pl-8 opacity-80 text-md">
+              2-5 people
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- components/website/home/Hero/Pitch/index.jsx
+++ components/website/home/Hero/Pitch/index.jsx
@@ -1,16 +1,17 @@
 import Button from "/components/utils/Button";
 
 export default function Pitch() {
-    return (
-        <div className="font-iextrabold w-full lg:w-9/12 xl:9/12 text-white">
-            <h2 className="text-4xl font-bold">
-                We gather speakers, attract parterns and give our imagination wings, all for this to be your favorite week.
-            </h2>
-            <div className="mt-8 w-56">
-                <a href="/team">
-                    <Button text="MEET THE TEAM" customStyle="text-white bg-primary border-tertiary hover:bg-tertiary" />
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full text-white font-iextrabold lg:w-9/12 xl:9/12">
+      <h2 className="text-4xl font-bold">
+        We gather speakers, attract parterns and give our imagination wings, all
+        for this to be your favorite week.
+      </h2>
+      <div className="w-56 mt-8">
+        {/*<a href="/team">
+                <Button text="MEET THE TEAM" customStyle="text-white bg-primary border-tertiary hover:bg-tertiary" />
+            </a>*/}
+      </div>
+    </div>
+  );
 }
