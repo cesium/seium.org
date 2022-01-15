e3b1f11b7e739eaa3b81bd289e0cf8bac937dd46
Filipe Felício <55213469+feliciofilipe@users.noreply.github.com>
2022-01-15 15:57:58 +0000

    Fix mascot animation sm (#225)

--- components/website/schedule/Hero/Title/index.jsx
+++ components/website/schedule/Hero/Title/index.jsx
@@ -7,7 +7,7 @@ export default function Title() {
                 Schedule
             </h5>
             <h1 className="font-iextrabold text-white text-6xl w-11/12 md:text-8xl md:w-full xl:text-9xl 2xl:w-4/5 relative z-0">
-                Five awesome days of learning, sharing and
+                Five awesome days of learning, sharing and &nbsp;
                 <UnderlineAnimation text="Did you see what I did there?" afterText="">winning</UnderlineAnimation>
             </h1>
         </div>
