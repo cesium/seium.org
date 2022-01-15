ba5139c5c396682d169689beb62ce135f3420d0c
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-01-15 15:47:08 +0000

    Fix mascot animation sm

--- components/website/utils/UnderlineAnimation/index.jsx
+++ components/website/utils/UnderlineAnimation/index.jsx
@@ -57,13 +57,13 @@ export default function UnderlineAnimation({children, text, afterText}) {
     return (
         <span className="relative leading-none w-auto h-auto inline-block z-10 mt-4">
             
-            <span className="relative h-auto inline-block z-10 border-b-8 border-white" onMouseEnter={() => updateState((oldState) => ({...oldState, status: 1}))} 
+            <span className="relative h-auto inline-block z-10 md:border-b-8 border-white" onMouseEnter={() => updateState((oldState) => ({...oldState, status: 1}))} 
                 onMouseLeave={() => updateState((oldState) => ({ ...oldState, status: 3}))}>
                     {children}
             </span>
             <span className="relative h-auto inline-block z-10">&nbsp;{afterText ? afterText : ""}</span>
             <div className="absolute top-full  w-96 h-52 z-10 overflow-y-hidden overflow-x-visible">
-            <div className="absolute top-0 left-20 pt-3 z-0 overflow-visible" style={{marginTop: `${st.margin}px`}}>      
+            <div className="hidden md:block absolute top-0 left-20 pt-3 z-0 overflow-visible" style={{marginTop: `${st.margin}px`}}>      
                 <Card img="/images/mascot-footer.svg" alt="MascotFooter" inverted={true}>
                     <h5 className={`font-ithin ${styles.cardText}`}>{text}</h5>
                 </Card>
