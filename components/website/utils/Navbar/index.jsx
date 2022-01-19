80edb86f890d6784acbcc486ea2ec6549ce2ca92
Maria <mariajbp00@gmail.com>
2022-01-19 22:16:08 +0000

    Add staff form to navbar

--- components/website/utils/Navbar/index.jsx
+++ components/website/utils/Navbar/index.jsx
@@ -13,6 +13,7 @@ const navigation = [
     // { name: 'Challenges', href: '/challenges' },
     { name: 'Speakers', href: '/speakers' },
     { name: 'FAQs', href: '/faq' },
+    { name: 'Join Staff', href: 'https://forms.gle/4EwD1Gs8FGCGRdYX8' }
     // { name: 'Login', href: '/login' },
 ]
 
@@ -23,17 +24,17 @@ export default function Navbar(props) {
                 <>
                     <div className={`spacing pt-14 pb-4 bg-${props.bgColor}`}>
                         <div className="flex items-center justify-between h-16">
-                            <div className="flex flex-auto z-50 relative">
-                                <div className="grid grid-cols-4 w-full">
+                            <div className="relative z-50 flex flex-auto">
+                                <div className="grid w-full grid-cols-4">
                                     <a href="/" className="">
                                         <Image src="/images/sei-logo.svg" width="50" height="40" />
                                     </a>
-                                    <div className="justify-self-end col-span-3 hidden lg:block">
+                                    <div className="hidden col-span-3 justify-self-end lg:block">
                                         <div className="flex flex-auto gap-x-20">
                                             <div className="grid grid-cols-3 gap-x-20 gap-y-6 xl:gap-y-0">
                                                 {navigation.map((item, i) => (
                                                     <a key={i}
-                                                        className="font-iregular text-sm text-white text-opacity-40 hover:text-opacity-100"
+                                                        className="text-sm text-white font-iregular text-opacity-40 hover:text-opacity-100"
                                                         href={item.href}
                                                     >
                                                         {item.name}
@@ -47,8 +48,8 @@ export default function Navbar(props) {
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex lg:hidden z-50 relative">
-                                <Disclosure.Button className="inline-flex items-center justify-center text-white w-6 h-6">
+                            <div className="relative z-50 flex lg:hidden">
+                                <Disclosure.Button className="inline-flex items-center justify-center w-6 h-6 text-white">
                                     {open ? (
                                         <FontAwesomeIcon icon={faTimes} />
                                     ) : (
@@ -60,12 +61,12 @@ export default function Navbar(props) {
                     </div>
 
                     <Disclosure.Panel className={`lg:hidden bg-${props.bgColor}`}>
-                        <div className="px-2 pt-12 object-cover min-h-screen z-50 relative">
+                        <div className="relative z-50 object-cover min-h-screen px-2 pt-12">
                             {navigation.map((item, i) => (
                                 <Disclosure.Button
                                     as="a"
                                     href={item.href}
-                                    className="text-white hover:text-quinary block text-center py-6 rounded-md text-3xl font-ibold"
+                                    className="block py-6 text-3xl text-center text-white rounded-md hover:text-quinary font-ibold"
                                 >
                                     {item.name}
                                 </Disclosure.Button>
