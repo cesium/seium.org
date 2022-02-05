338ee2fa00671f073e59692bca4d60fef6b0d8aa
Filipe Felício <55213469+feliciofilipe@users.noreply.github.com>
2022-02-05 15:40:39 +0000

    :art: Improve navbar logo (#258)

--- components/website/utils/Navbar/index.jsx
+++ components/website/utils/Navbar/index.jsx
@@ -33,7 +33,7 @@ export default function Navbar({ bgColor, fgColor, button, children }) {
                   <Link href="/" passHref>
                     <div className={styles.logo}>
                       <Image
-                        className="cursor-pointer"
+                        className="opacity-60 hover:opacity-100 cursor-pointer"
                         src="/images/sei-logo.svg"
                         width="50"
                         height="40"
