2e9808abe13e717465561322dea1bf5d24af96bd
ruioliveira02 <rpoliveira02@gmail.com>
2022-02-07 13:44:46 +0000

    Fix undefined attendee

--- components/moonstone/user/badgedex/Badge/index.jsx
+++ components/moonstone/user/badgedex/Badge/index.jsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 export default function Badge({ name, id, avatar, tokens }) {
   return (
-    <div
-    // onClick={handleClick}
-    >
-      <div>
-        <img src={avatar} className="" alt="Error"></img>
-      </div>
-      <div className="grid grid-rows-2 justify-items-center font-iregular">
-        <div>{name}</div>
-        <div>{tokens} 💰 </div>
-      </div>
-    </div>
+    <Link href={`/badge/${id}`}>
+      <a className="h-full w-full">
+        <div>
+          <img src={avatar} className="" alt="Error"></img>
+        </div>
+        <div className="grid grid-rows-2 justify-items-center font-iregular">
+          <div>{name}</div>
+          <div>{tokens} 💰 </div>
+        </div>
+      </a>
+    </Link>
   );
 }
