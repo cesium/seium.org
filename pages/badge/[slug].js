d77dae95976b2fc86199f1a583828bf75143bd70
Nelson Estevão <nelsonmestevao@gmail.com>
2022-02-10 23:56:14 +0000

    Add badge name to badge page
    Closes #291.

--- pages/badge/[slug].js
+++ pages/badge/[slug].js
@@ -8,14 +8,11 @@ import Dashboard from "/components/moonstone/user/utils/Dashboard";
 import Heading from "/components/moonstone/utils/Heading";
 import Badge from "/components/moonstone/user/badgedex/Badge";
 
-function BadgeOwner({ user, badge, when }) {
+function Owner({ attendee }) {
   return (
     <div className="border-b-solid mb-5 grid w-full grid-cols-2 border-b-2 border-slate-400 pb-3">
       <div className="text-left">
-        <p className="font-ibold">{user}</p>
-      </div>
-      <div className="text-right">
-        <p className="text-iregular text-quinary">{badge}</p>
+        <p className="font-ibold">{attendee.nickname || attendee.name}</p>
       </div>
     </div>
   );
@@ -24,25 +21,16 @@ function BadgeOwner({ user, badge, when }) {
 function BadgeSlug() {
   const [badge, updateBadge] = useState({});
   const router = useRouter();
+  const { slug } = router.query;
   const maxUsersToShow = 5;
 
   useEffect(() => {
-    getBadge(router.query.slug)
+    getBadge(slug)
       .then((response) => {
         updateBadge(response.data.data);
       })
       .catch((_) => router.replace("/404"));
-  }, []);
-
-  const owners =
-    badge && badge.attendees
-      ? badge.attendees.map(
-          (entry, id) =>
-            id < maxUsersToShow && (
-              <BadgeOwner key={id} user={entry.name} badge={badge.name} />
-            )
-        )
-      : [];
+  }, [slug]);
 
   return (
     <Dashboard>
@@ -55,17 +43,22 @@ function BadgeSlug() {
 
       <div className="mt-10 grid grid-cols-1 justify-items-center gap-y-8 gap-x-2 lg:grid-cols-2">
         <div className="w-full">
-          <Heading text="Badge info">
-            <span className="mt-1 mr-12 font-iregular">{badge.name}</span>
-          </Heading>
-
+          <Heading text="Badge info" />
           <Badge {...badge} />
         </div>
 
         <div className="w-full">
           <Heading text="Owners" />
-
-          <div className="mt-10">{owners}</div>
+          <div className="mt-10">
+            {badge && badge.attendees
+              ? badge.attendees.map(
+                  (attendee, i) =>
+                    i < maxUsersToShow && (
+                      <Owner key={attendee.id} attendee={attendee} />
+                    )
+                )
+              : []}
+          </div>
         </div>
       </div>
     </Dashboard>
