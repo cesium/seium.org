414e6d12a3cca224fcf34879398f8bc2f0c00f36
ruioliveira02 <rpoliveira02@gmail.com>
2022-02-11 17:51:34 +0000

    Basic implementation

--- pages/attendee/profile.js
+++ pages/attendee/profile.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { withAuth, useAuth } from "/components/Auth";
 
 import Form from "/components/moonstone/utils/Form";
@@ -22,18 +24,35 @@ function getFirstName(fullName) {
 function Profile() {
   const { user } = useAuth();
 
+  const [editing, setEditing] = useState(false);
+  const [username, setUsername] = useState(user.nickname || "");
+
+  const companyBadges = user.badges.filter(entry => entry.type == 4).length;
+  const level = companyBadges == 19 ? 4 : Math.floor(companyBadges / 20);
+  const neededBadges = (level == 3) ? 19 - companyBadges : (level + 1) * 5 - companyBadges;
+
+  const levelEntries = [10, 30, 60, 100];
+
   return (
     <Dashboard
       href="profile"
       title={`Hello, ${getFirstName(user.name)} 👋`}
       description={`Welcome to your profile!`}
     >
+      {JSON.stringify(user)}
       <div className="mt-12 grid-cols-2 overflow-hidden">
         <div className="col-span-1 float-left w-full xl:w-1/2">
           <Heading text="User Profile">
-            <div className="w-20">
-              <button className="w-full items-center rounded-full border border-quinary bg-quinary px-4 py-1 text-center font-iregular text-sm text-secondary shadow-sm">
-                Edit
+            <div className="w-auto">
+              <button className="w-full items-center rounded-full border border-quinary bg-quinary px-4 py-1 text-center font-iregular text-sm text-secondary shadow-sm"
+                onClick={() => {
+                  if(editing) {
+                    
+                  } else {
+                    setEditing(true);
+                  }               
+                }}>
+                {editing ? "Save Changes" : "Edit"}
               </button>
             </div>
           </Heading>
@@ -47,29 +66,14 @@ function Profile() {
           </div>
           <Form>
             <Input
-              text="NAME"
-              id="name"
-              name="name"
-              value={user.name || ""}
-              bgColor="white"
-              fgColor="black"
-            />
-            <Input
               text="USERNAME"
               id="username"
               name="username"
-              value={user.nickname || ""}
+              value={username}
               bgColor="white"
               fgColor="black"
-            />
-            <Input
-              text="PASSWORD"
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              bgColor="white"
-              fgColor="black"
+              enabled={editing}
+              onChange={e => setUsername(e.currentTarget.value)}
             />
 
             <button className="inline-block h-auto pl-6 pb-5 text-quinary underline">
@@ -84,19 +88,19 @@ function Profile() {
 
             <div className="grid-cols-2 overflow-hidden">
               <div className="col-span-1 float-left w-1/2 font-iregular">
-                💰170 Tokens
+                💰{user.token_balance} Tokens
               </div>
               <div className="col-span-1 float-left w-1/2 font-iregular">
-                🏅68 Badges
+                🏅{user.badge_count} Badges
               </div>
             </div>
 
             <div className="mt-4 grid-cols-2 overflow-hidden">
               <div className="col-span-1 float-left w-1/2 font-iregular">
-                🏆16 Entries Final Draw
+                🏆{user.entries} Entries Final Draw
               </div>
               <div className="col-span-1 float-left w-1/2 font-iregular">
-                🏁Level 3 Checkpoint
+                🏁Level {level} Checkpoint
               </div>
             </div>
           </div>
@@ -104,40 +108,52 @@ function Profile() {
           <div className="mt-10">
             <Heading text="Checkpoints"></Heading>
             <p className="font-iregular">
-              <b className="font-ibold">Level 1</b> 5 companies &rarr; +10
+              <b className="font-ibold">Level 1</b> 5 companies &rarr; +{levelEntries[0]}
               entries
             </p>
             <p className="font-iregular">
-              <b className="font-ibold">Level 2</b> 10 companies &rarr; +20
+              <b className="font-ibold">Level 2</b> 10 companies &rarr; +{levelEntries[1]}
               entries
             </p>
             <p className="font-iregular">
-              <b className="font-ibold">Level 3</b> 15 companies &rarr; +40
+              <b className="font-ibold">Level 3</b> 15 companies &rarr; +{levelEntries[2]}
               entries
             </p>
             <p className="font-iregular">
-              <b className="font-ibold">Level 4</b> 20 companies &rarr; +100
+              <b className="font-ibold">Level 4</b> 19 companies &rarr; +{levelEntries[3]}
               entries
             </p>
 
-            <CheckpointTracker checkpoints={5} progress={3} />
+            <CheckpointTracker checkpoints={4} progress={level} />
 
-            <p className="font-iregular">
-              You just need 4 more bages to go to Level 4 (and win 10+ entries
+            {level != 4 && <><p className="font-iregular">
+              You just need {neededBadges} more bages to go to Level {level + 1} (and win +{levelEntries[level]} entries
               to the final draw). Hurry!
             </p>
-            <CodeInput />
+            <CodeInput /></>}
           </div>
 
           <div className="mt-10">
             <Heading text="Upload CV"></Heading>
             <p className="font-iregular">
               {" "}
-              Get a chance to win a spot at the Coporate dinner by submiting you
+              Get a chance to win a spot at the Corporate dinner by submiting you
               CV!
             </p>
 
-            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:pt-5">
+            <a href="mailto:cv@seium.org" className="inline-block h-auto pl-6 pb-5 text-quinary underline">SEND YOUR CV</a>
+          </div>
+        </div>
+      </div>
+    </Dashboard>
+  );
+}
+
+export default withAuth(Profile);
+
+
+/*
+<div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:pt-5">
               <div className="mt-1 sm:col-span-2 sm:mt-0">
                 <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
                   <div className="space-y-1 text-center">
@@ -175,11 +191,4 @@ function Profile() {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </Dashboard>
-  );
-}
-
-export default withAuth(Profile);
+*/
