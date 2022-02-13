23b27672a97436e4d92cce57fae57812112d6fa4
Miguel Brandão <mickael.a.brandao@gmail.com>
2022-02-13 17:16:11 +0000

    Fix partners (#322)

--- components/website/home/Partners/index.jsx
+++ components/website/home/Partners/index.jsx
@@ -8,22 +8,19 @@ export default function Partners() {
       <h2 className="py-10 text-center font-iextrabold text-5xl text-white lg:text-6xl">
         Partners who made this possible
       </h2>
-      <div className="my-10 grid grid-cols-1 gap-10 lg:grid-cols-4">
+      <div className="my-10 flex flex-wrap items-center justify-center gap-10">
         {partners.map((partner, i) => (
           <div
-            className={`${partners.length == 2 && "col-span-2"}
-                                     ${partners.length == 1 && "col-span-4"}
-                                     m-auto grayscale filter hover:filter-none`}
+            className="m-auto w-40 grayscale filter hover:filter-none"
             key={i}
           >
             <a href={partner.url} target="_blank" rel="noreferrer">
-              <button>
-                <Image
-                  src={`/images/partners/${partner.image}`}
-                  width="200"
-                  height="200"
-                />
-              </button>
+              <Image
+                src={`/images/partners/${partner.image}`}
+                width="300"
+                height="300"
+                alt={partner.name}
+              />
             </a>
           </div>
         ))}
