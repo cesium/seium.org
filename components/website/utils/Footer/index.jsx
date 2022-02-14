77663772f709592c7d4312fcc69ed6e50d1a571f
Luis <44007026+LAraujo7@users.noreply.github.com>
2022-02-14 03:07:44 +0000

    Add Link tag around the a one

--- components/website/utils/Footer/index.jsx
+++ components/website/utils/Footer/index.jsx
@@ -12,13 +12,15 @@ import styles from "./style.module.css";
 
 function DefaultAnimation() {
   return (
-    <p>
+    <span>
       Psst. Have you checked the{" "}
-      <a href="/challenges" className="text-primary underline">
+      <Link href="/challenges">
+        <a className="underline text-primary">
         challenges?
-      </a>{" "}
+        </a>
+      </Link>{" "}
       Just saying.
-    </p>
+    </span>
   );
 }
 
@@ -91,8 +93,8 @@ export default function Footer(props) {
             </p>
           </div>
         </div>
-        <div className="flex-2 py-10">
-          <div className="grid grid-rows-2 justify-items-center gap-10 font-iregular text-sm text-white lg:grid-cols-2 lg:justify-items-end">
+        <div className="py-10 flex-2">
+          <div className="grid grid-rows-2 gap-10 text-sm text-white justify-items-center font-iregular lg:grid-cols-2 lg:justify-items-end">
             <Link passHref href="https://2021.seium.org/">
               <a className="text-white hover:underline">Previous Edition</a>
             </Link>
@@ -113,7 +115,7 @@ export default function Footer(props) {
           </div>
         </div>
       </div>
-      <div className="invisible -mt-20 flex justify-center pb-10 lg:visible">
+      <div className="flex justify-center invisible pb-10 -mt-20 lg:visible">
         <Animation
           text={
             props.animationText != undefined ? (
