13f674698665b07bc981544d7ceb6408dc7e5da6
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-12-09 19:11:28 +0000

    Refactor Speakers page

---
+++ layout/Speakers/Speakers.tsx
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { withoutAuth } from "@context/Auth";
+
+import { Hero, Schedule } from "./components";
+
+import Navbar from "@components/Navbar";
+import Footer from "@components/Footer";
+
+function Speakers() {
+  const router = useRouter();
+
+  return (
+    <Navbar bgColor="secondary" button="quinary" fgColor="white">
+      <Hero />
+      <Schedule
+        color="secondary"
+        detailed={false}
+        filters={router.query.speaker}
+      />
+      <Footer color="secondary" />
+    </Navbar>
+  );
+}
+
+export default withoutAuth(Speakers);
