d2ff6e41207c69dc62f32bfc23405a6a17d57e66
tiago-bacelar <pereira.migueltiago@gmail.com>
2023-02-06 14:48:01 +0000

    Added unmount to gif creator

--- layout/Team/components/Organization/Animation/index.jsx
+++ layout/Team/components/Organization/Animation/index.jsx
@@ -4,10 +4,12 @@ import seilogo from "./logo_animation.json";
 
 export default function Animation() {
   useEffect(() => {
-    Lottie.loadAnimation({
+    const x = Lottie.loadAnimation({
       container: document.querySelector("#sei-logo"),
       animationData: seilogo,
     });
+
+    return () => x.destroy();
   }, []);
 
   return <div id="sei-logo" height={150} width={150} />;
