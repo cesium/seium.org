14e7efe84f7fc04e8335929ce1a674a088d96ad5
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-02-11 18:33:31 +0000

    reduce gap between socials

--- components/JoinUs/index.jsx
+++ components/JoinUs/index.jsx
@@ -1,7 +1,7 @@
 export default function JoinUs(props) {
   return (
     <a
-      href="https://sei22.eventbrite.pt"
+      href="https://sei23.eventbrite.pt"
       className={`-mt-5 flex h-28 w-28 rotate-15 transform items-center justify-center font-ibold text-xl`}
     >
       <img src="/images/join-us.svg" alt="Join us" />
