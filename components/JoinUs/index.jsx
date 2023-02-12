8ea1fe630e5a7eebbc88d10e3a77d465a42f12f5
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-02-12 20:40:37 +0000

    Fix Join Us button (#466)

--- components/JoinUs/index.jsx
+++ components/JoinUs/index.jsx
@@ -1,9 +1,10 @@
 export default function JoinUs(props) {
   return (
-    <>
-      <a href="https://sei23.eventbrite.pt">
-        <img src="/images/join-us.svg" alt="Join us" />
-      </a>
-    </>
+    <a
+      href="https://sei23.eventbrite.pt"
+      className={`flex h-28 w-28 flex-shrink-0 rotate-15 transform items-center justify-center font-ibold text-xl text-${props.fgColor} bg-${props.button} translate-x-0 rounded-full`}
+    >
+      Join us 👋
+    </a>
   );
 }
