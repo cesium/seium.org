a3d50133cc65857e463cf19f5bf2fb5a42a583e9
Enzo Vieira <enzovieira16@hotmail.com>
2023-10-24 16:50:32 +0100

    fix: Text size, misspeling and animation duration

--- layout/Home/components/Hero/Title/index.tsx
+++ layout/Home/components/Hero/Title/index.tsx
@@ -9,29 +9,22 @@ export default function Title() {
       {/* 2xl:leading-[6.5rem] is intended to only work with the following font - Terminal */}
       <h1
         aria-label="The software engineering week is back, let's just SEI that."
-        className="font-terminal-uppercase h-72 w-11/12 text-4xl text-white xs:text-5xl sm:text-6xl md:h-96 md:w-full md:text-7xl lg:text-8xl 2xl:w-5/6 2xl:text-9xl 2xl:leading-[6.5rem]"
+        className="font-terminal-uppercase h-72 w-11/12 text-4xl text-white xs:text-5xl sm:text-6xl md:h-96 md:w-full md:text-7xl lg:text-8xl 2xl:w-5/6 2xl:leading-[6.5rem]"
       >
         <TypeWriter
           aria-hidden
           onInit={(typewriter) => {
             typewriter
-              .typeString("The ")
-              .typeString("sotware")
-              .changeDeleteSpeed(20)
-              .pauseFor(100)
-              .deleteChars(7)
-              .typeString("<span>software<br/>engeen</span>")
-              .deleteChars(3)
-              .typeString("<span>ineering week is back, let&apos;s  </span>")
+              .typeString("The software engineering week is back, let's ")
               .typeString("<span style='white-space: nowrap;'>just say</span>") // since this lib do not accept "&nbsp;", css "white-space: nowrap" will do the same
-              .pauseFor(400)
+              .pauseFor(200)
               .deleteChars(3)
               .typeString("<u style='text-underline-offset: 8px;'>SEI</u>")
               .typeString(" that.")
               .start();
           }}
           options={{
-            delay: 50,
+            delay: 25,
           }}
         />
       </h1>
