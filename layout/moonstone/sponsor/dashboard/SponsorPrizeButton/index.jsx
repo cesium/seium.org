644aec268f49d4c9624d6afd4b46458322c73d4c
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-11-10 08:46:56 +0000

    chore: use a Button component

--- layout/moonstone/sponsor/dashboard/SponsorPrizeButton/index.jsx
+++ layout/moonstone/sponsor/dashboard/SponsorPrizeButton/index.jsx
@@ -1,9 +1,9 @@
+import Button from "@components/Button";
+
 export default function SponsorPrizeButton({ prize }) {
   return (
     <div className="mt-8 w-auto">
-      <button className="m-auto block h-16 w-full rounded-full bg-quinary">
-        <p className="font-iregular"> 🏅 Prémio: {prize} </p>
-      </button>
+      <Button customStyle="m-auto block h-16 w-full rounded-full bg-quinary" title={`🏅  Prémio ${prize}`}/>
     </div>
   );
 }
