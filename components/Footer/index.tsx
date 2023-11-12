82726d391ce05630277ab9a4cff96205cba1fef1
Dário Guimarães <dsgdevbraga@gmail.com>
2023-11-12 18:22:53 +0000

    Fixed wrap

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -116,16 +116,13 @@ export default function Footer(props: IFooterProps) {
         </div>
 
         <div className="flex-2">
-          <div className="grid grid-rows-2 justify-items-center gap-8 font-iregular text-sm text-white lg:grid-cols-2 lg:justify-items-end">
+          <div className="grid grid-rows-2 justify-items-center gap-8 font-iregular text-sm text-white whitespace-nowrap lg:grid-cols-2 lg:justify-items-start">
             <Link
               href="https://2022.seium.org/"
               className="text-white hover:underline"
             >
               Previous Edition
-            </Link>
-            <Link href="/docs/regulamento.pdf" className="hover:underline">
-              General Regulation
-            </Link>
+            </Link> 
             <Link
               href="https://docs.google.com/forms/d/e/1FAIpQLSdV1bSyW2tcLuTC_jJCGdZ5NZHUlgETK7nQkOmyDzwb7eFS4Q/viewform"
               className="hover:underline"
@@ -134,6 +131,9 @@ export default function Footer(props: IFooterProps) {
             </Link>
             <Link href="/docs/survival.pdf" className="hover:underline">
               Survival Guide
+            </Link>
+            <Link href="/docs/regulamento.pdf" className="hover:underline">
+              General Regulation
             </Link>
           </div>
           <div className="flex justify-center lg:justify-end">
