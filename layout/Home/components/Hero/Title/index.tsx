ca112cee2f832344c29637d39218cb65f12a78bb
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-12-03 15:56:17 +0000

    fix: deploy preview 2

--- layout/Home/components/Hero/Title/index.tsx
+++ layout/Home/components/Hero/Title/index.tsx
@@ -13,7 +13,9 @@ export default function Title() {
         aria-label="The software engineering week is back, let's just SEI that."
         className="font-terminal-uppercase h-72 w-11/12 text-4xl text-white xs:text-5xl sm:text-6xl md:h-96 md:w-full md:text-7xl lg:text-8xl 2xl:w-5/6 2xl:leading-[6.5rem]"
       >
-        {TypeEffect()}
+        <div>
+          {TypeEffect()}
+        </div>
       </h1>
     </div>
   );
