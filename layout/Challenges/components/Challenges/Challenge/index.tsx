e0cef160b438cb9b6865c9aa4a6e59da63b428e0
João Lobo <joao@lobo.is>
2023-12-04 11:20:01 +0000

    feat: animate challenges page components

--- layout/Challenges/components/Challenges/Challenge/index.tsx
+++ layout/Challenges/components/Challenges/Challenge/index.tsx
@@ -1,5 +1,5 @@
 import Button from "@components/Button";
-import Link from "next/link";
+import { motion as Motion } from "framer-motion";
 
 function Action({ text, url }) {
   return (
@@ -33,65 +33,72 @@ interface IProps {
 
 export default function Challenge(props: IProps) {
   return (
-    <div className="sticky top-60">
-      <div>
-        <h2 className="font-terminal-uppercase text-3xl text-white md:text-4xl xl:text-5xl">
-          {props.title}
-        </h2>
-        <p className="mt-10 font-iregular text-white">{props.description}</p>
-        <div className="mt-3 flex flex-col">
-          {props.hrefs &&
-            props.hrefs.map((href, i) => (
-              <a
-                href={href.url}
-                key={i}
-                target="_blank"
-                rel="noreferrer"
-                className="font-iregular text-quinary hover:underline"
-              >
-                {href.name ?? href.url}
-              </a>
-            ))}
-        </div>
+    <Motion.div
+      initial={{ y: 30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: "easeInOut", duration: 0.3 }}
+      key={props.title}
+    >
+      <div className="sticky top-60">
         <div>
-          {props.prizes && (
-            <h3 className="text-ibold md:text-md xl:text-md mt-5 mb-3 text-xl text-white ">
-              Awards 🏆
-            </h3>
-          )}
-          {props.prizes &&
-            props.prizes.map((prize, index) => {
-              let ordinal = "";
-              switch (index + 1) {
-                case 1:
-                  ordinal = "st";
-                  break;
-                case 2:
-                  ordinal = "nd";
-                  break;
-                case 3:
-                  ordinal = "rd";
-                  break;
-                default:
-                  ordinal = "th";
-              }
-              return (
-                <p key={index}>
-                  <a
-                    href={props.prizes[index].url}
-                    className="text-iregular text-quinary"
-                  >
-                    {index + 1}
-                    <sup>{ordinal}</sup> place - {props.prizes[index].name}
-                  </a>
-                </p>
-              );
-            })}
-          {props.button != null && (
-            <Action text={props.button.text} url={props.button.url} />
-          )}
+          <h2 className="font-terminal-uppercase text-3xl text-white md:text-4xl xl:text-5xl">
+            {props.title}
+          </h2>
+          <p className="mt-10 font-iregular text-white">{props.description}</p>
+          <div className="mt-3 flex flex-col">
+            {props.hrefs &&
+              props.hrefs.map((href, i) => (
+                <a
+                  href={href.url}
+                  key={i}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="font-iregular text-quinary hover:underline"
+                >
+                  {href.name ?? href.url}
+                </a>
+              ))}
+          </div>
+          <div>
+            {props.prizes && (
+              <h3 className="text-ibold md:text-md xl:text-md mt-5 mb-3 text-xl text-white ">
+                Awards 🏆
+              </h3>
+            )}
+            {props.prizes &&
+              props.prizes.map((prize, index) => {
+                let ordinal = "";
+                switch (index + 1) {
+                  case 1:
+                    ordinal = "st";
+                    break;
+                  case 2:
+                    ordinal = "nd";
+                    break;
+                  case 3:
+                    ordinal = "rd";
+                    break;
+                  default:
+                    ordinal = "th";
+                }
+                return (
+                  <p key={index}>
+                    <a
+                      href={props.prizes[index].url}
+                      className="text-iregular text-quinary"
+                    >
+                      {index + 1}
+                      <sup>{ordinal}</sup> place - {props.prizes[index].name}
+                    </a>
+                  </p>
+                );
+              })}
+            {props.button != null && (
+              <Action text={props.button.text} url={props.button.url} />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Motion.div>
   );
 }
