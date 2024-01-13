9cecdd2a285046652bbccc36c0f1146a9ac900e5
João Lobo <joao@lobo.is>
2024-01-13 17:29:20 +0000

    feat: change button color on mobile

--- components/JoinUs/index.jsx
+++ components/JoinUs/index.jsx
@@ -3,7 +3,7 @@ export default function JoinUs(props) {
     <a
       href="https://sei24.eventbrite.pt"
       target="_blank"
-      className="before:ease font-terminal-uppercase relative flex h-10 w-28 flex-wrap content-center justify-center overflow-hidden rounded-full border-2 bg-transparent text-white transition-transform before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-quinary before:transition-all before:duration-300 hover:scale-105 hover:text-white hover:before:-rotate-180"
+      className="before:ease font-terminal-uppercase relative flex h-10 w-28 flex-wrap content-center justify-center overflow-hidden rounded-full border-2 border-white bg-quinary text-white transition-transform before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-quinary before:transition-all before:duration-300 hover:scale-105 hover:text-white hover:before:-rotate-180 lg:bg-transparent"
     >
       <span className="relative z-10">Join Us</span>
     </a>
