9ae5e9b8b7999138505ad709c30044bf3fabd2bf
João Lobo <joao@lobo.is>
2024-01-13 16:59:02 +0000

    fix: button animation

--- components/JoinUs/index.jsx
+++ components/JoinUs/index.jsx
@@ -3,7 +3,7 @@ export default function JoinUs(props) {
     <a
       href="https://sei24.eventbrite.pt"
       target="_blank"
-      className={`before:ease flex flex-wrap content-center justify-center border-2 text-${props.fgColor} font-terminal-uppercase relative h-10 w-28 overflow-hidden rounded-full bg-transparent before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-${props.button} transition-transform before:transition-all before:duration-300 hover:scale-105 hover:text-white hover:before:-rotate-180`}
+      className="before:ease font-terminal-uppercase relative flex h-10 w-28 flex-wrap content-center justify-center overflow-hidden rounded-full border-2 bg-transparent text-white transition-transform before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-quinary before:transition-all before:duration-300 hover:scale-105 hover:text-white hover:before:-rotate-180"
     >
       <span className="relative z-10">Join Us</span>
     </a>
