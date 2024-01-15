fd598876a75cd9433e1aa12ddf06ebe8dbc0b563
Felicio <filipe.felicio.1999@gmail.com>
2024-01-15 07:31:54 +0000

    feat: add twitter social

--- components/Social/index.jsx
+++ components/Social/index.jsx
@@ -4,12 +4,13 @@ import {
   faInstagram,
   faGithubAlt,
   faLinkedinIn,
+  faTwitter
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Social() {
   return (
-    <div className="mt-4 grid grid-cols-5 justify-items-center gap-x-3 md:gap-x-6">
+    <div className="mt-4 flex items-center space-x-3 md:space-x-6">
       <a
         className="w-5 opacity-50 hover:opacity-100"
         href="https://www.instagram.com/sei.uminho"
@@ -33,6 +34,14 @@ export default function Social() {
         rel="noreferrer"
       >
         <FontAwesomeIcon icon={faGithubAlt} />
+      </a>
+      <a
+        className="w-5 opacity-50 hover:opacity-100"
+        href="https://twitter.com/cesiuminho"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faTwitter} />
       </a>
       <a
         className="w-5 opacity-50 hover:opacity-100"
