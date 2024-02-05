76d9e8ed707d51c948d3b57b3014c652d8f2bb25
João Lobo <joao@lobo.is>
2024-02-05 02:19:31 +0000

    feat: remove mascot from login and signup pages

--- layout/SignUp/SignUp.tsx
+++ layout/SignUp/SignUp.tsx
@@ -27,21 +27,10 @@ function Signup() {
   return (
     <div className="min-h-screen select-none overflow-hidden bg-secondary">
       <Return componentStyle="sm:ml-14 mt-10 sm:mt-20 mb-6" />
-      <div className="flex flex-col items-center justify-center sm:mt-16">
+      <div className="mb-4 flex flex-col items-center justify-center sm:mt-16">
         <Title text="Sign up" />
         <SignUpForm courses={courses} />
         <Text text="Already have an account?" link="Login here" href="/login" />
-        <div className="fixed bottom-0 right-60 hidden lg:block">
-          <Motion.div
-            initial={{ y: 30, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 1 }}
-          >
-            <Card img="/images/void.svg" alt="MascotFooter" inverted={false}>
-              Very restricted area. You just need to fill the form.
-            </Card>
-          </Motion.div>
-        </div>
       </div>
     </div>
   );
