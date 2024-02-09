a6c815c6baefddcaaf57f66584cb2a36921bcd5c
João Lobo <joao@lobo.is>
2024-02-09 23:24:43 +0000

    feat: final thanks

--- layout/Home/components/Hero/Title/index.tsx
+++ layout/Home/components/Hero/Title/index.tsx
@@ -26,14 +26,8 @@ export default function Title() {
       >
         <h1 className="font-terminal-uppercase relative z-20 w-11/12 text-4xl text-white xs:text-5xl sm:text-6xl md:w-full md:text-7xl lg:text-8xl 2xl:w-5/6 2xl:text-9xl 2xl:leading-[6.5rem]">
           <span className="relative z-20">
-            The software engineering week is back, let&apos;s just&nbsp;
+            Thank you! See you all next year! {"<3"}
           </span>
-          <UnderlineAnimation
-            text="Did you see what I did there?"
-            afterText="that."
-          >
-            SEI
-          </UnderlineAnimation>
         </h1>
       </Motion.div>
     </div>
