37290ed7626fa60f795dbcda45872a1650c262f7
João Lobo <30907944+joaodiaslobo@users.noreply.github.com>
2024-02-26 16:58:58 +0000

    chore: re-add hero title (#726)

--- layout/Home/components/Hero/Title/index.tsx
+++ layout/Home/components/Hero/Title/index.tsx
@@ -26,8 +26,14 @@ export default function Title() {
       >
         <h1 className="font-terminal-uppercase relative z-20 w-11/12 text-4xl text-white xs:text-5xl sm:text-6xl md:w-full md:text-7xl lg:text-8xl 2xl:w-5/6 2xl:text-9xl 2xl:leading-[6.5rem]">
           <span className="relative z-20">
-            Thank you! See you all next year! {"<3"}
+            The software engineering week is back, let&apos;s just&nbsp;
           </span>
+          <UnderlineAnimation
+            text="Did you see what I did there?"
+            afterText="that."
+          >
+            SEI
+          </UnderlineAnimation>
         </h1>
       </Motion.div>
     </div>
